f264b3be4edd75dd54beb9be6126f67ebfbad8cf
MitchRegan <mitchell.p.regan@gmail.com>
2022-07-01 09:49:25 -0500

    Completed year 2020 day 7

--- AdventOfCode/AdventOfCodePython.py
+++ AdventOfCode/AdventOfCodePython.py
@@ -5,7 +5,8 @@ def y2020():
     #from Year2020 import Day3
     #from Year2020 import Day4
     #from Year2020 import Day5
-    from Year2020 import Day6
+    #from Year2020 import Day6
+    from Year2020 import Day7
 
 
 def y2021():
