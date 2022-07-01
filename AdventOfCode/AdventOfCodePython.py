00a67be7fd85102922d3833d2562218141158852
MitchRegan <mitchell.p.regan@gmail.com>
2022-07-01 02:13:49 -0500

    - Forgot to include the 2020 day 4 input files - Fixed a folder init file for 2020

--- AdventOfCode/AdventOfCodePython.py
+++ AdventOfCode/AdventOfCodePython.py
@@ -1,19 +1,32 @@
-print("===================== Year 2021 =====================")
-#from Year2021 import Day1
-#from Year2021 import Day2
-#from Year2021 import Day3
-#from Year2021 import Day4
-#from Year2021 import Day5
-#from Year2021 import Day6
-#from Year2021 import Day7
-#from Year2021 import Day8
-#from Year2021 import Day9
-#from Year2021 import Day10
-#from Year2021 import Day11
-#from Year2021 import Day12
-#from Year2021 import Day13
-#from Year2021 import Day14
-#from Year2021 import Day15
-from Year2021 import Day16
-#from Year2021 import Day17
-#from Year2021 import Day25
+def y2020():
+    print("===================== Year 2020 =====================")
+    #from Year2020 import Day1
+    #from Year2020 import Day2
+    #from Year2020 import Day3
+    #from Year2020 import Day4
+
+
+def y2021():
+    print("\n\n===================== Year 2021 =====================")
+    #from Year2021 import Day1
+    #from Year2021 import Day2
+    #from Year2021 import Day3
+    #from Year2021 import Day4
+    #from Year2021 import Day5
+    #from Year2021 import Day6
+    #from Year2021 import Day7
+    #from Year2021 import Day8
+    #from Year2021 import Day9
+    #from Year2021 import Day10
+    #from Year2021 import Day11
+    #from Year2021 import Day12
+    #from Year2021 import Day13
+    #from Year2021 import Day14
+    #from Year2021 import Day15
+    from Year2021 import Day16
+    #from Year2021 import Day17
+    #from Year2021 import Day25
+
+
+y2020()
+y2021()
