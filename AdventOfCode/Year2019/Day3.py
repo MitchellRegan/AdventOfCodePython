335d4751e921dca7917fde5876502427b3b5dda2
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2022-12-04 02:37:56 -0600

    - FINALLY solved both parts of year 2019 day 3 (also cleaned up the code for part 1 significantly)

--- AdventOfCode/Year2019/Day3.py
+++ AdventOfCode/Year2019/Day3.py
@@ -5,139 +5,119 @@
 inFile = "C:\\Users\\Mitch\\source\\repos\\AdventOfCodePython\\AdventOfCode\\Year2019\\InputRealFiles\\d3_real.txt"
 
 
-def printInput(coords):
-    #Finding the bounds of the grid to use
-    T = 0
-    B = 0
-    L = 0
-    R = 0
-
-    for i in coords[0]:
-        if i[0] < L:
-            L = i[0]
-        if i[0] > R:
-            R = i[0]
-        if i[1] < B:
-            B = i[1]
-        if i[1] > T:
-            T = i[1]
-
-    for i in coords[1]:
-        if i[0] < L:
-            L = i[0]
-        if i[0] > R:
-            R = i[0]
-        if i[1] < B:
-            B = i[1]
-        if i[1] > T:
-            T = i[1]
-
-    # Initializing a blank grid with the rows and columns needed
-    grid = []
-    for r in range(abs(T) + abs(B) + 2):
-        row = ['.'] * (abs(L) + abs(R) + 2)
-        grid.append(row)
-
-    crossings = []
-
-    #Mapping out all of the coordinates for the first line
-    for a in range(0, len(coords[0])-1):
-        xs = [coords[0][a][0] + abs(L), coords[0][a+1][0] + abs(L)]
-        ys = [coords[0][a][1] + abs(B)+1, coords[0][a+1][1] + abs(B)+1]
-
-        #If the x coordinates are the same, we draw a vertical line
-        if xs[0] == xs[1]:
-            for yval in range(min(ys), max(ys)):
-                grid[yval][xs[0]] = '|'
-        #If the y coordinates are the same, we draw a horizontal line
-        if ys[0] == ys[1]:
-            for xval in range(min(xs), max(xs)):
-                grid[ys[0]][xval] = '-'
-                
-        grid[ys[1]][xs[1]] = '+'
-        grid[ys[0]][xs[0]] = '+'
-
-    #Mapping out all of the coordinates for the second line
-    for a in range(0, len(coords[1])-1):
-        xs = [coords[1][a][0] + abs(L), coords[1][a+1][0] + abs(L)]
-        ys = [coords[1][a][1] + abs(B)+1, coords[1][a+1][1] + abs(B)+1]
-
-        #If the x coordinates are the same, we draw a vertical line
-        if xs[0] == xs[1]:
-            for yval in range(min(ys), max(ys)):
-                if grid[yval][xs[0]] == '.':
-                    grid[yval][xs[0]] = 'I'
-                elif grid[yval][xs[0]] == '|' or grid[yval][xs[0]] == '-':
-                    grid[yval][xs[0]] = 'X'
-                    crossings.append((xs[0] - abs(L), yval - abs(B)-1))
-        #If the y coordinates are the same, we draw a horizontal line
-        if ys[0] == ys[1]:
-            for xval in range(min(xs), max(xs)):
-                if grid[ys[0]][xval] == '.':
-                    grid[ys[0]][xval] = '='
-                elif grid[ys[0]][xval] == '|' or grid[ys[0]][xval] == '-':
-                    grid[ys[0]][xval] = 'X'
-                    crossings.append((xval - abs(L), ys[0] - abs(B)-1))
-                    
-        grid[ys[0]][xs[0]] = '#'
-        grid[ys[1]][xs[1]] = '#'
-
-    #Setting the starting point to O
-    grid[abs(B)+1][abs(L)] = 'O'
-
-
-    dists = []
-    for c in crossings:
-        dists.append((abs(c[0]) + abs(c[1])))
-
-    return min(dists)
-
-
 def solution1():
-    #2D list to contain lists for each wire, each of which contains a list of coordinate points
-    wires = []
+    #dictionary where the key is the coordinate point, and the value is the ID of the wire at that point
+    wirePoints = {"0,0":'O'}
 
+    wireNum = 1
+    minDist = float('inf')
     #Looping through each line in the input file
     with open(inFile, 'r') as f:
         for line in f:
             #delineating the path instructions for this wire (line)
             instructions = line.split(',')
-
-            #Creating a list of coordinate points for where this wire turns, starting at point (0,0)
-            wirePoints = [(0,0)]
-
-            # Looping through each instruction to trace the points where the wire turns
-            for i in instructions:
-                if i[0] == 'U':
-                    x = wirePoints[-1][0]
-                    y = wirePoints[-1][1] + int(i[1:])
-                    wirePoints.append((x,y))
-                elif i[0] == 'D':
-                    x = wirePoints[-1][0]
-                    y = wirePoints[-1][1] - int(i[1:])
-                    wirePoints.append((x,y))
-                elif i[0] == 'L':
-                    x = wirePoints[-1][0] - int(i[1:])
-                    y = wirePoints[-1][1]
-                    wirePoints.append((x,y))
-                elif i[0] == 'R':
-                    x = wirePoints[-1][0] + int(i[1:])
-                    y = wirePoints[-1][1]
-                    wirePoints.append((x,y))
+            x = 0
+            y = 0
+            for w in instructions:
+                xdir = 0
+                ydir = 0
+                if w[0] == 'U':
+                    ydir = 1
+                elif w[0] == 'D':
+                    ydir = -1
+                elif w[0] == 'L':
+                    xdir = -1
+                elif w[0] == 'R':
+                    xdir = 1
                 else:
-                    print("ERROR: invalid instruction", i)
-                    return 0
+                    print("Invalid instruction:", w)
 
-            # Adding this wire's coordinate points to the list of all wires
-            wires.append(wirePoints)
+                newX = x + (xdir * int(w[1:]))
+                newY = y + (ydir * int(w[1:]))
 
-    # it is NOT 1285. correct answer is 870
-    
-    return printInput(wires)
+                # Looping through every point between the current and previous xy points
+                for yval in range(min([y,newY]), max([y,newY])+1):
+                    for xval in range(min([x,newX]), max([x,newX])+1):
+                        point = str(xval)+","+str(yval)
+
+                        #Checking if the current position is empty
+                        if point not in wirePoints.keys():
+                            wirePoints[point] = str(wireNum)
+                        elif wirePoints[point] != str(wireNum) and wirePoints[point] != 'O':
+                            wirePoints[point] = 'X'
+                            #Checking if the manhattan distance here is less than the current minimum
+                            dist = abs(xval) + abs(yval)
+                            if dist < minDist:
+                                minDist = dist
+                # Updating the stored x,y vals for the start of the next instruction
+                x = newX
+                y = newY
+
+            wireNum += 1
+
+    return minDist
 
 
 def solution2():
-    return 0
+    #Dictionary where they key is the xy coordinate and the value is an ordered pair for the wire ID and the step distance
+    wirePoints = {"0,0":[0,0]}
+
+    wireNum = 1
+    minDist = float('inf')
+    #Looping through each line in the input file
+    with open(inFile, 'r') as f:
+        for line in f:
+            #delineating the path instructions for this wire (line)
+            instructions = line.split(',')
+            x = 0
+            y = 0
+            step = 0
+            for w in instructions:
+                xdir = 0
+                ydir = 0
+                if w[0] == 'U':
+                    ydir = 1
+                elif w[0] == 'D':
+                    ydir = -1
+                elif w[0] == 'L':
+                    xdir = -1
+                elif w[0] == 'R':
+                    xdir = 1
+                else:
+                    print("Invalid instruction:", w)
+
+                newX = x + (xdir * int(w[1:]))
+                xstep = 1
+                if xdir == -1:
+                    xstep = -1
+                newY = y + (ydir * int(w[1:]))
+                ystep = 1
+                if ydir == -1:
+                    ystep = -1
+                # Looping through every point between the current and previous xy points
+                for yval in range(y, newY+ystep, ystep):
+                    for xval in range(x, newX+xstep, xstep):
+                        point = str(xval)+","+str(yval)
+
+                        #Checking if the current position is empty
+                        if point not in wirePoints.keys():
+                            wirePoints[point] = [wireNum, step]
+                        elif wirePoints[point][0] != wireNum and wirePoints[point][0] != 0:
+                            dist = step + wirePoints[point][1]
+                            wirePoints[point] = [wireNum, dist]
+                            #Checking if the total step distance is less than the current minimum
+                            if dist < minDist:
+                                minDist = dist
+
+                        step += 1
+                # Updating the stored x,y vals for the start of the next instruction
+                x = newX
+                y = newY
+                step -= 1
+
+            wireNum += 1
+
+    return minDist
 
     
 print("Year 2019, Day 3 solution part 1:", solution1())
