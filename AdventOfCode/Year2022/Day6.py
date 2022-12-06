9f23c9798b285a8327748a3ea56cb031a762eb83
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2022-12-05 23:51:58 -0600

    - Uploading my completed solution for year 2022 day 6 - Uploading the real and test input files for day 6 as well

--- AdventOfCode/Year2022/Day6.py
+++ AdventOfCode/Year2022/Day6.py
@@ -1,22 +1,37 @@
 #https://adventofcode.com/2022/day/5
 #https://adventofcode.com/2022/day/5#part2
 
-inFile = "C:\\Users\\Mitch\\source\\repos\\AdventOfCodePython\\AdventOfCode\\Year2022\\InputTestFiles\\d6_test.txt"
-#inFile = "C:\\Users\\Mitch\\source\\repos\\AdventOfCodePython\\AdventOfCode\\Year2022\\InputRealFiles\\d6_real.txt"
+#inFile = "C:\\Users\\Mitch\\source\\repos\\AdventOfCodePython\\AdventOfCode\\Year2022\\InputTestFiles\\d6_test.txt"
+inFile = "C:\\Users\\Mitch\\source\\repos\\AdventOfCodePython\\AdventOfCode\\Year2022\\InputRealFiles\\d6_real.txt"
 
 def solution1():
+    strIn = ""
     #Looping through each line in the file
     with open(inFile, 'r') as f:
         for line in f:
-            print(1)
+            strIn += line
+
+    for i in range(3,len(strIn)):
+        if strIn[i:i+4].count(strIn[i]) == 1 and strIn[i:i+4].count(strIn[i+1]) == 1 and strIn[i:i+4].count(strIn[i+2]) == 1 and strIn[i:i+4].count(strIn[i+3]) == 1:
+            return i+4
     return 0
 
 
 def solution2():
+    strIn = ""
     #Looping through each line in the file
     with open(inFile, 'r') as f:
         for line in f:
-            print(1)
+            strIn += line
+
+    for i in range(3,len(strIn)):
+        isValid = True
+        for j in range(i, i+14):
+            if strIn[i:i+14].count(strIn[j]) > 1:
+                isValid = False
+                break
+        if isValid:
+            return i+14
     return 0
 
 
