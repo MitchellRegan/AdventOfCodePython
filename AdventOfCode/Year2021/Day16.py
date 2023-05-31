584b64571caa4fbd393bc25b0c14639da62e49a4
MitchRegan <mitchell.p.regan@gmail.com>
2023-05-31 06:52:54 -0500

    - Completed year 2021 day 16 part 1

--- AdventOfCode/Year2021/Day16.py
+++ AdventOfCode/Year2021/Day16.py
@@ -1,11 +1,21 @@
 #https://adventofcode.com/2021/day/16
 #https://adventofcode.com/2021/day/16#part2
 
-# Real data
-#data = "220D69802BE00A0803711E1441B1006E39C318A12730C200DCE66D2CCE360FA0055652CD32966E3004677EDF600B0803B1361741510076254138D8A00E4FFF3E3393ABE4FC7AC10410010799D2A4430003764DBE281802F3102CA00D4840198430EE0E00021D04E3F41F84AE0154DFDE65A17CCBFAFA14ADA56854FE5E3FD5BCC53B0D2598027A00848C63F2B918C7E513DEC3290051B3867E009CCC5FE46BD520007FE5E8AD344B37583D0803E40085475887144C01A8C10FE2B9803B0720D45A3004652FD8FA05F80122CAF91E5F50E66BEF8AB000BB0F4802039C20917B920B9221200ABF0017B9C92CCDC76BD3A8C4012CCB13CB22CDB243E9C3D2002067440400D9BE62DAC4D2DC0249BF76B6F72BE459B279F759AE7BE42E0058801CC059B08018A0070012CEC045BA01006C03A8000D46C02FA000A8EA007200800E00618018E00410034220061801D36BF178C01796FC52B4017100763547E86000084C7E8910AC0027E9B029FE2F4952F96D81B34C8400C24AA8CDAF4F1E98027C00FACDE3BA86982570D13AA640195CD67B046F004662711E989C468C01F1007A10C4C8320008742287117C401A8C715A3FC2C8EB3777540048272DFE7DE1C0149AC8BC9E79D63200B674013978E8BE5E3A2E9AA3CCDD538C01193CFAB0A146006AA00087C3E88B130401D8E304A239802F39FAC922C0169EA3248DF2D600247C89BCDFE9CA7FFD8BB49686236C9FF9795D80C0139BEC4D6C017978CF78C5EB981FCE7D4D801FA9FB63B14789534584010B5802F3467346D2C1D1E080355B00424FC99290C7E5D729586504803A2D005E677F868C271AA479CEEB131592EE5450043A932697E6A92C6E164991EFC4268F25A294600B5002A3393B31CC834B972804D2F3A4FD72B928E59219C9C771EC3DC89D1802135C9806802729694A6E723FD6134C0129A019E600"
-# Test data
-#data = "D2FE28"
-data = "38006F45291200"
+import os
+inFileDir = os.path.dirname(__file__)
+#inFile = os.path.join(inFileDir, "InputTestFiles/d16_test.txt")
+inFile = os.path.join(inFileDir, "InputRealFiles/d16_real.txt")
+
+
+def getInput():
+    inputString = ""
+    with open(inFile, 'r') as f:
+        for line in f:
+            if line[-1] == '\n':
+                line = line[:-1]
+            inputString = line
+            break
+    return inputString
 
 
 def hexToBin(c_):
@@ -59,81 +69,95 @@ def binToDec(b_):
     return val
 
 
-def courseOfAction(str, bitsToAnalyze = -1):
-    return 0
-
-
-def parseLiteral(str):
-    # String to hold the binary value for the literal
-    binary = ''
-
-    while True:
-        print(" - -", str)
-        # if the first bit of this segment is a 0, that means it's the end of the packet
-        if str[0] is '0':
-            print(" - - - End of packet. Segment:", str[0] + str[1] + str[2] + str[3] + str[4])
-            binary += str[1] + str[2] + str[3] + str[4]
-            str = str[5:]
-            break
-        # If the first bit of this segment is a 1, that means the following 4 bits should be used for the binary literal
-        else:
-            print(" - - - Leading 1. Segment:", str[0] + str[1] + str[2] + str[3] + str[4])
-            binary += str[1] + str[2] + str[3] + str[4]
-            str = str[5:]
-
-    # If there's a required length
-    print(" - - Binary:", binary, "=", binToDec(binary))
-
-
-def parseOperator(str):
-    #If the first bit of this segment is 0, that means the next 15 bits represent the length of bits in this sub-packet
-    if str[0] is '0':
-        print(" - - - Leading 0. Determining size of sub-packets using next 15 bits")
-        binary = ''.join(str[1:16])
-        print(" - - Binary:", binary, "=", binToDec(binary))
-        str = str[16:]
-    else:
-        print(" - - - Leading 1. Determining size of sub-packets using next 11 bits")
-        binary = ''.join(str[1:12])
-        print(" - - Binary:", binary, "=", binToDec(binary))
-        str = str[12:]
-
-
-def solution1():
+def packetParser1(pkt_, debug_=False):
+    '''Recursive function to find the version sum of the given packet and all sub-packets it may contain.
+    returns: Int for the sum of all packet/sub-packet version numbers.
+    '''
     versionSum = 0
-
-    # Converting the string to binary
-    str = ""
-    for c in data:
-        str += hexToBin(c)
-    print(str)
-
-    while len(str) > 10:
+    #If the packet has a length less than 11, we can't do anything with it
+    while len(pkt_) > 10:
         # Getting the packet version from the first 3 bits
-        version = str[:3]
+        version = pkt_[:3]
         versionVal = binToDec(version)
         versionSum += versionVal
 
         # Getting the packet ID from the next 3 bits
-        print(str)
-        id = '' + str[3] + str[4] + str[5]
+        id = '' + pkt_[3] + pkt_[4] + pkt_[5]
         idVal = binToDec(id)
-        str = str[6:]
-
-        print("Version:", version, "=", versionVal)
-        print("ID:", id, "=", idVal)
-        print("Remainder:", str)
+        pkt_ = pkt_[6:]
+        
+        if debug_:
+            print("-------------------------------------------------")
+            print(pkt_)
+            print("VVVTTT")
+            print("Version:", version, "=", versionVal)
+            print("Type ID:", id, "=", idVal)
+            print("Remainder:", pkt_)
 
         # If the ID is 4, it's a literal
         if idVal == 4:
-            print(" - Literal:")
-            parseLiteral(str)
+            if debug_:
+                print("\tLiteral:")
+            # String to hold the binary value for the literal
+            binary = ''
+
+            while True:
+                if debug_:
+                    print("\t\t -", pkt_)
+                # if the first bit of this segment is a 0, that means it's the end of the packet
+                if pkt_[0] is '0':
+                    if debug_:
+                        print("\t\t - - End of packet. Segment:", pkt_[0] + pkt_[1] + pkt_[2] + pkt_[3] + pkt_[4])
+                    binary += pkt_[1] + pkt_[2] + pkt_[3] + pkt_[4]
+                    pkt_ = pkt_[5:]
+                    break
+                # If the first bit of this segment is a 1, that means the following 4 bits should be used for the binary literal
+                else:
+                    if debug_:
+                        print("\t\t - - Leading 1. Segment:", pkt_[0] + pkt_[1] + pkt_[2] + pkt_[3] + pkt_[4])
+                    binary += pkt_[1] + pkt_[2] + pkt_[3] + pkt_[4]
+                    pkt_ = pkt_[5:]
+
+            # If there's a required length
+            if debug_:
+                print("\t\t - Binary:", binary, "=", binToDec(binary))
         # Otherwise it's an operator
         else:
-            print(" - Operation")
-            parseOperator(str)
+            if debug_:
+                print("\tOperation")
+            #If the first bit of this segment is 0, that means the next 15 bits represent the length of bits in this sub-packet
+            if pkt_[0] is '0':
+                binary = ''.join(pkt_[1:16])
+                binVal = binToDec(binary)
+                if debug_:
+                    print(" - - - Leading 0. Determining size of sub-packets using next 15 bits")
+                    print(" - - Binary:", binary, "=", binVal)
+                pkt_ = pkt_[16:]
+                versionSum += packetParser1(pkt_[:binVal], debug_)
+                pkt_ = pkt_[binVal:]
+            else:
+                binary = ''.join(pkt_[1:12])
+                binVal = binToDec(binary)
+                if debug_:
+                    print(" - - - Leading 1. Determining number of sub-packets using next 11 bits")
+                    print(" - - Binary:", binary, "=", binVal)
+                pkt_ = pkt_[12:]
 
     return versionSum
+
+
+def solution1():
+    inputStr = getInput()
+    #print("Input string:", inputStr)
+
+    # Converting the string to binary
+    str = ""
+    for c in inputStr:
+        str += hexToBin(c)
+    #print("Input to binary:", str)
+
+    #Using the recursive packet parser function to find the sum of all version numbers
+    return packetParser1(str)
 
 
 def solution2():
