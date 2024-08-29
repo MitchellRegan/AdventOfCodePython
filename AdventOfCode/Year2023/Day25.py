f9d4672353f096cc84c935b36507515cc41c6037
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-08-28 23:58:36 -0500

    Finally went back to re-visit the problems from 2023 that I took a break from, and completed day 25 part 1

--- AdventOfCode/Year2023/Day25.py
+++ AdventOfCode/Year2023/Day25.py
@@ -31,7 +31,19 @@ def getInput():
     return wires
 
 
-def bfsDist(graph:dict, edge:list)->int:
+def bfsDist(graph:dict, edge:tuple)->int:
+    '''Using Breadth-First Search to find the minimum distance between components if the wire connecting them is severed.
+    
+    Parameters
+    ----------
+        graph: Dictionary where keys are strings for component names, and values are the list of connected components.
+        edge: Tuple of (component 1, component 2) denoting a wire connection between components.
+        
+    Returns
+    ----------
+        Int for the minimum distance between the components.
+    '''
+    
     start = edge[0]
     end = edge[1]
     
@@ -50,6 +62,46 @@ def bfsDist(graph:dict, edge:list)->int:
             if e not in dist.keys():
                 q.append(e)
                 dist[e] = dist[head] + 1
+                
+    return -1 #Default value if error found
+
+
+def groupSizes(graph:dict)->list:
+    '''Removes the three wire connections from the graph and finds the sizes of each group of remaining connections.
+    
+    Parameters
+    ----------
+        graph: Dictionary where keys are strings for component names, and values are the list of connected components. This is a copy of the original, but with wires already severed.
+        
+    Returns
+    ----------
+        2D list of strings where each sub-list contains all components in a connected group.
+    '''
+    
+    components = [x for x in graph.keys()]
+    componentGroups = []
+    while len(components) > 0:
+        #If components still exist in the components list, they weren't found in previous groups of connected components
+        newGroup = [components.pop(0)]
+        q = graph[newGroup[0]]
+        # Performing a bfs search 
+        while len(q) > 0:
+            head = q.pop(0)
+            #If we've seen this component before, ignore it and move on
+            if head in newGroup:
+                continue
+            #If the component hasn't been seen yet, we remove it from the unseen component list, add it to this group's list, and add it's connections to the queue
+            else:
+                newGroup.append(head)
+                components.remove(head)
+                for conn in graph[head]:
+                    if conn not in q and conn in components:
+                        q.append(conn)
+            
+        componentGroups.append(newGroup)
+        
+    return componentGroups
+                
 
 
 def solution1():
@@ -63,55 +115,44 @@ def solution1():
         for v in wires[u]:
             if (min(u,v), max(u,v)) not in edges.keys():
                 e = (min(u,v), max(u,v))
-                edges[e] = 0
+                edges[e] = bfsDist(wires, e)
 
-    for e in edges.keys():
-        print(e)
-    return
-
-    removed = 0
-    while removed < 3:
-        #Tracking which edges will be removed and their max distance weight
-        removedEdges = []
-        maxDist = 0
-
-        #Perform a bfs search from u to v without being able to take the direct route
-        #The weight of edge (u,v) is equal to the bfs distance
-        for e in edges:
-            edges[e] = bfsDist(wires, e)
-
-            if edges[e] > maxDist:
-                maxDist = edges[e]
-                removedEdges = [e]
-            elif edges[e] == maxDist:
-                removedEdges.append(e)
-
-        #Removing the top 3 edges with the highest distance weight
-        for e in removedEdges:
-            edges.pop(e)
-            wires[e[0]].remove(e[1])
-            wires[e[1]].remove(e[0])
-            removed += 1
-            print("Removing edge", e)
-            if removed == 3:
-                print("TOO MANY EDGES. REMAINING EDGES:", removedEdges)
-                break
-
-            
-    for point in wires.keys():
-        #Doing a bfs search from this point to all others in the connected group
-        seen = {point:True}
-        q = [point]
-        while len(q) > 0:
-            head = q.pop(0)
-            for c in wires[head]:
-                if c not in seen.keys():
-                    seen[c] = True
-                    q.append(c)
-        print("Total points:", len(wires.keys()))
-        print("Points in group:", len(seen.keys()))
-        print("Points in other group:", len(wires.keys()) - len(seen.keys()))
-        return (len(wires.keys()) - len(seen.keys())) * len(seen.keys())
+    #for e in edges.keys():
+    #    print(e, edges[e])
+    
+    distances = [(e, edges[e]) for e in edges.keys()]
+    distances.sort(key=lambda x: x[1], reverse=True)
+    #for d in distances:
+    #    print(d)
+    
+    topIndexes = 5
+    bestSize = 0
+    for i1 in range(0, topIndexes-2):
+        for i2 in range(i1+1, topIndexes-1):
+            for i3 in range(i2+1, topIndexes):
+                #Creating a copy of the graph of connections and severing each of the 3 chosen wires
+                newGraph = {}
+                for k in wires.keys():
+                    newGraph[k] = [x for x in wires[k]]
+                    
+                newGraph[distances[i1][0][0]].remove(distances[i1][0][1])
+                newGraph[distances[i1][0][1]].remove(distances[i1][0][0])
+                
+                newGraph[distances[i2][0][0]].remove(distances[i2][0][1])
+                newGraph[distances[i2][0][1]].remove(distances[i2][0][0])
+                
+                newGraph[distances[i3][0][0]].remove(distances[i3][0][1])
+                newGraph[distances[i3][0][1]].remove(distances[i3][0][0])
+                
+                wireGroups = groupSizes(newGraph)
+                
+                #We need to find the largest product of group sizes as long as there's 2 separate groups
+                if len(wireGroups) > 1:
+                    groupSize = len(wireGroups[0]) * len(wireGroups[1])
+                    if groupSize > bestSize:
+                        bestSize = groupSize
+                        
+    return bestSize
 
 
 def solution2():
