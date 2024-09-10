0aaad06c66de15089f421694247bdddf13af0036
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-09-10 17:58:20 -0500

    Completed year 2020 day 24 parts 1 and 2 Completed year 2020 day 16 part 1, and almost done with part 2

--- AdventOfCode/Year2020/Day16.py
+++ AdventOfCode/Year2020/Day16.py
@@ -1,34 +1,121 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    locations = {}
+    ticket = []
+    nearby = []
 
+    fieldType = 0 #0 = "location", 1 = "your ticket", 2 = "nearby tickets"
     with open(inFile, 'r') as f:
         for line in f:
             line = line.replace('\n', '')
 
-    return inpt
+            #Getting locations
+            if fieldType == 0:
+                if len(line) == 0:
+                    fieldType += 1
+                else:
+                    locName = line.split(": ")[0]
+                    range1 = line.split(' ')[-3].split('-')
+                    range2 = line.split(' ')[-1].split('-')
+                    locations[locName] = [int(range1[0]), int(range1[1]), int(range2[0]), int(range2[1])]
+            #Your ticket
+            elif fieldType == 1:
+                if len(line) == 0:
+                    fieldType += 1
+                elif line != "your ticket:":
+                    line = line.split(',')
+                    ticket = [int(x) for x in line]
+            #Nearby tickets
+            elif fieldType == 2:
+                if line != "nearby tickets:":
+                    line = line.split(',')
+                    nearby.append([int(x) for x in line])
+    return locations, ticket, nearby
             
 
 def solution1():
-    inpt = getInput()
+    locations, ticket, nearby = getInput()
     
-    
-
-    return
+    ans = 0
+    #Checking every value in every ticket
+    for t in nearby:
+        for val in t:
+            inRange = False
+            #Comparing the value against the ranges in the locations map
+            for k in locations:
+                if (locations[k][0] <= val and val <= locations[k][1]) or (locations[k][2] <= val and val <= locations[k][3]):
+                    inRange = True
+                    break
+            if not inRange:
+                ans += val
+    return ans
 
 
 def solution2():
-    inpt = getInput()
+    locations, ticket, nearby = getInput()
     
-    return
+    ticketIndex = 0
+    while ticketIndex < len(nearby):
+        testing and print("Checking ticket", nearby[ticketIndex])
+        #Checking every value in every ticket
+        for val in nearby[ticketIndex]:
+            inRange = False
+            #Comparing the value against the ranges in the locations map
+            for k in locations:
+                if (locations[k][0] <= val and val <= locations[k][1]) or (locations[k][2] <= val and val <= locations[k][3]):
+                    inRange = True
+                    testing and print("\t\tIn Range")
+                    break
+            if not inRange:
+                testing and print("\t", val, "not in ranges", locations[k][0], "-", locations[k][1], "or", locations[k][2], "-", locations[k][3])
+                print("Popping index", ticketIndex)
+                nearby.pop(ticketIndex)
+            else:
+                ticketIndex += 1
+               
+    testing and print("\nRemaining:")
+    for n in nearby:
+        testing and print(n)
+        
+    locIndex = {}
+    while len(locIndex.keys()) < len(ticket):
+        for loc in locations.keys():
+            validIndex = [i for i in range(0, len(nearby[0]))]
+            testing and print(loc, "possible valid indeces:", validIndex)
+            for n in nearby:
+                for i in range(0, len(n)):
+                    #Removing any indeces we know for certain aren't available
+                    if i in locIndex.keys() and i in validIndex:
+                        testing and print("\t\t", i, "already taken by", locIndex[i])
+                        validIndex.remove(i)
+                        testing and print("\t\tAfter remove")
+                    #Removing any indeces whose values don't match with the known ranges
+                    elif n[i] not in range(locations[loc][0], locations[loc][1]+1) and n[i] not in range(locations[loc][2], locations[loc][3]+1):
+                        testing and print("\t\t", i, "not in ranges", locations[loc])
+                        if i in validIndex:
+                            validIndex.remove(i)
+                        testing and print("\t\tAfter remove")
+                    
+            testing and print("\tRemaining indeces:", validIndex)
+            if len(validIndex) == 1:
+                locIndex[validIndex[0]] = loc
+                testing and print(loc, "at index", validIndex[0])
+                
+        testing and print("Location indeces:", locIndex, '\n====================================================')
+        
+    ans = 1
+    for i in locIndex.keys():
+        if locIndex[i].split(' ')[0] == 'departure':
+            ans *= ticket[i]
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
--- AdventOfCode/Year2020/Day24.py
+++ AdventOfCode/Year2020/Day24.py
@@ -1,34 +1,147 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
     inpt = []
-
+    
     with open(inFile, 'r') as f:
         for line in f:
             line = line.replace('\n', '')
-
+            inpt.append(line)
     return inpt
             
 
 def solution1():
     inpt = getInput()
     
-    
-
-    return
+    tiles = {}
+    for line in inpt:
+        i = 0 #Index for the current instruction in the line
+        x = 0.0 #Row, col position of our current location
+        y = 0.0
+        
+        while i < len(line):
+            if line[i] == 'e':
+                i += 1
+                x += 1
+            elif line[i] == 'w':
+                i += 1
+                x -= 1
+            elif line[i:i+2] == 'se':
+                i += 2
+                x += 0.5
+                y -= 1
+            elif line[i:i+2] == 'sw':
+                i += 2
+                x -= 0.5
+                y -= 1
+            elif line[i:i+2] == 'ne':
+                i += 2
+                x += 0.5
+                y += 1
+            elif line[i:i+2] == 'nw':
+                i += 2
+                x -= 0.5
+                y += 1
+                
+        testing and print("Final position:", (x,y))
+        if (x,y) not in tiles.keys():
+            tiles[(x,y)] = True
+        else:
+            tiles[(x,y)] = not tiles[(x,y)]
+            
+    ans = 0
+    for x in tiles.keys():
+        if tiles[x]:
+            ans += 1
+    return ans
 
 
 def solution2():
     inpt = getInput()
     
-    return
+    #Getting the initial state for the flipped tiles
+    tiles = {}
+    for line in inpt:
+        i = 0 #Index for the current instruction in the line
+        x = 0.0 #Row, col position of our current location
+        y = 0.0
+        
+        while i < len(line):
+            if line[i] == 'e':
+                i += 1
+                x += 1
+            elif line[i] == 'w':
+                i += 1
+                x -= 1
+            elif line[i:i+2] == 'se':
+                i += 2
+                x += 0.5
+                y -= 1
+            elif line[i:i+2] == 'sw':
+                i += 2
+                x -= 0.5
+                y -= 1
+            elif line[i:i+2] == 'ne':
+                i += 2
+                x += 0.5
+                y += 1
+            elif line[i:i+2] == 'nw':
+                i += 2
+                x -= 0.5
+                y += 1
+                
+        if (x,y) not in tiles.keys():
+            tiles[(x,y)] = True
+        else:
+            tiles[(x,y)] = not tiles[(x,y)]
+            
+        #Adding the adjacent tiles as white (False) so we can check them later
+        for adj in [(x+1,y), (x+0.5,y+1), (x-0.5,y+1), (x-1,y), (x-0.5,y-1), (x+0.5,y-1)]:
+            if adj not in tiles.keys():
+                tiles[adj] = False
+    
+    #Applying the flipping rules based on adjacent tiles
+    for day in range(0, 100):
+        newTiles = {}
+        
+        for tile in tiles.keys():
+            x,y = tile
+            adjacentBlack = 0
+            for adj in [(x+1,y), (x+0.5,y+1), (x-0.5,y+1), (x-1,y), (x-0.5,y-1), (x+0.5,y-1)]:
+                #Counting the number of black tiles around the current tile
+                if adj in tiles.keys() and tiles[adj]:
+                    adjacentBlack += 1
+
+                #Adding all adjacent tiles as white (False) to check later
+                if adj not in newTiles.keys():
+                    newTiles[adj] = False
+                    
+            if tiles[tile]:
+                if adjacentBlack == 0 or adjacentBlack > 2:
+                    newTiles[tile] = False
+                else:
+                    newTiles[tile] = True
+            else:
+                if adjacentBlack == 2:
+                    newTiles[tile] = True
+                else:
+                    newTiles[tile] = False
+
+        tiles = newTiles
+        
+    #Counting the number of black (True) tiles
+    ans = 0    
+    for k in tiles.keys():
+        if tiles[k]:
+           ans += 1 
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
