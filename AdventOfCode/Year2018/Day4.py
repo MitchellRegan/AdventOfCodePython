e4446e8d543af63c23fd76a9a7e95bae4fb6781c
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-09-14 16:34:04 -0500

    Completed year 2018 days 4 and 5, both parts

--- AdventOfCode/Year2018/Day4.py
+++ AdventOfCode/Year2018/Day4.py
@@ -1,10 +1,13 @@
+from statistics import harmonic_mean
+
+
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
@@ -12,23 +15,172 @@ def getInput():
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
-
+            line = line.replace('\n', '').replace('[', '').replace(']', '').split(' ')
+            year = int(line[0].split('-')[0])
+            mon = int(line[0].split('-')[1])
+            day = int(line[0].split('-')[2])
+            hour = int(line[1].split(':')[0])
+            minute = int(line[1].split(':')[1])
+            state = ""
+            if line[2] == "Guard":
+                state = int(line[3].replace('#', ''))
+            elif line[2] == "falls":
+                state = "sleep"
+            else:
+                state = "awake"
+                
+            inpt.append([year, mon, day, hour, minute, state])
     return inpt
             
 
 def solution1():
     inpt = getInput()
     
-    
+    #Sorting the entries based on their times (using bubble-sort I know it's bad)
+    for i in range(0, len(inpt)-1):
+        for j in range(i+1, len(inpt)):
+            yr1,mo1,dy1,hr1,mn1,st1 = inpt[i]
+            yr2,mo2,dy2,hr2,mn2,st2 = inpt[j]
+            min1 = mn1 + (hr1 * 60) + (dy1 * 1440)
+            min2 = mn2 + (hr2 * 60) + (dy2 * 1440)
 
-    return
+            if yr1 > yr2 or (yr1 == yr2 and mo1 > mo2) or (yr1 == yr2 and mo1 == mo2 and min1 > min2):
+                placeholder = inpt[i]
+                inpt[i] = inpt[j]
+                inpt[j] = placeholder
+            
+    
+    prevMin = None
+    guardID = None
+    guardState = "awake"
+    guardTimes = {}
+    for line in inpt:
+        testing and print(line)
+        yr,mo,dy,hr,mn,state = line
+        curMin = mn
+        if hr != 0:
+            curMin = 0
+        
+        #Setting our initial guard/time state
+        if guardID is None:
+            guardID = state
+            prevMin = curMin
+            guardState = "awake"
+            continue
+        
+        #If the previous state was sleep, we keep track of all minutes he was asleep up til this point
+        if guardState == "sleep":
+            for x in range(prevMin, curMin):
+                gt = (guardID, x)
+                if gt in guardTimes.keys():
+                    guardTimes[gt] += 1
+                else:
+                    guardTimes[gt] = 1
+
+        #If there's a change in guard shifts
+        if type(state) is int:
+            testing and print("\tSwitching to guard", state)
+            guardID = state
+            guardState = "awake"
+        else:
+            guardState = state
+            
+        prevMin = curMin
+        
+    #Finding the guard that slept the most number of minutes
+    guardSleepCount = {}
+    bestGuard = None
+    for x in guardTimes.keys():
+        g,m = x
+        if g in guardSleepCount.keys():
+            guardSleepCount[g] += guardTimes[x]
+        else:
+            guardSleepCount[g] = guardTimes[x]
+            
+        if bestGuard is None or guardSleepCount[g] > guardSleepCount[bestGuard]:
+            bestGuard = g
+            
+    testing and print("\nGuard sleep counts:", guardSleepCount, "\n\tBest Guard:", bestGuard, ":", guardSleepCount[bestGuard])
+    
+    #Finding the minute that the sleepiest guard slept the most
+    sleepMinCount = {}
+    bestMin = None
+    for x in guardTimes.keys():
+        g,m = x
+        if g != bestGuard:
+            continue
+        
+        if m not in sleepMinCount.keys():
+            sleepMinCount[m] = guardTimes[x]
+        else:
+            sleepMinCount[m] += guardTimes[x]
+            
+        if bestMin is None or sleepMinCount[m] > sleepMinCount[bestMin]:
+            bestMin = m
+            
+    testing and print("\tBest Minute:", bestMin, ":", sleepMinCount[bestMin])
+    return bestMin * bestGuard
 
 
 def solution2():
     inpt = getInput()
     
-    return
+    #Sorting the entries based on their times (using bubble-sort I know it's bad)
+    for i in range(0, len(inpt)-1):
+        for j in range(i+1, len(inpt)):
+            yr1,mo1,dy1,hr1,mn1,st1 = inpt[i]
+            yr2,mo2,dy2,hr2,mn2,st2 = inpt[j]
+            min1 = mn1 + (hr1 * 60) + (dy1 * 1440)
+            min2 = mn2 + (hr2 * 60) + (dy2 * 1440)
+
+            if yr1 > yr2 or (yr1 == yr2 and mo1 > mo2) or (yr1 == yr2 and mo1 == mo2 and min1 > min2):
+                placeholder = inpt[i]
+                inpt[i] = inpt[j]
+                inpt[j] = placeholder
+            
+    prevMin = None
+    guardID = None
+    guardState = "awake"
+    guardTimes = {}
+    for line in inpt:
+        yr,mo,dy,hr,mn,state = line
+        curMin = mn
+        if hr != 0:
+            curMin = 0
+        
+        #Setting our initial guard/time state
+        if guardID is None:
+            guardID = state
+            prevMin = curMin
+            guardState = "awake"
+            continue
+        
+        #If the previous state was sleep, we keep track of all minutes he was asleep up til this point
+        if guardState == "sleep":
+            for x in range(prevMin, curMin):
+                gt = (guardID, x)
+                if gt in guardTimes.keys():
+                    guardTimes[gt] += 1
+                else:
+                    guardTimes[gt] = 1
+
+        #If there's a change in guard shifts
+        if type(state) is int:
+            guardID = state
+            guardState = "awake"
+        else:
+            guardState = state
+            
+        prevMin = curMin
+        
+    #Finding the most consistent minute that any guard was asleep for
+    bestGuardMin = None
+    for x in guardTimes.keys():
+        if bestGuardMin is None or guardTimes[x] > guardTimes[bestGuardMin]:
+            bestGuardMin = x
+            
+    testing and print("Guard", bestGuardMin[0], "was asleep on min", bestGuardMin[1], guardTimes[bestGuardMin], "times")        
+    return bestGuardMin[0] * bestGuardMin[1]
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
--- AdventOfCode/Year2018/Day5.py
+++ AdventOfCode/Year2018/Day5.py
@@ -1,34 +1,96 @@
+from collections import deque
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = ""
 
     with open(inFile, 'r') as f:
         for line in f:
             line = line.replace('\n', '')
-
+            inpt = line
     return inpt
             
 
 def solution1():
     inpt = getInput()
     
-    
+    stack = deque()
+    stack.append(inpt[0])
+    testing and print("Input:", inpt, "\nStack:", stack)
+    for i in range(1, len(inpt)):
+        #If stack is empty, nothing to compare
+        if len(stack) == 0:
+            stack.append(inpt[i])
+            continue
+        
+        cur = inpt[i]
+        prev = stack[-1]
+        testing and print("\tIndex:", i, "    Prev: ", prev, "    Cur:", cur)
+        #If the current letter is upper-case and the previous letter is lower-case
+        if ord(cur) <= 90 and ord(prev) >=  97 and ord(cur) == ord(prev)-32:
+            testing and print("\t\tSame letter. Destroying")
+            stack.pop()
+        #If the current letter is lower-case and the previous letter is upper-case
+        elif ord(cur) >= 97 and ord(prev) <= 90 and ord(cur)-32 == ord(prev):
+            testing and print("\t\tSame letter. Destroying")
+            stack.pop()
+        #No match, so adding it to the stack
+        else:
+            stack.append(inpt[i])
 
-    return
+    return len(stack)
 
 
 def solution2():
     inpt = getInput()
     
-    return
+    #Iterating through all letters in the alphabet to see which one works best when removed
+    bestLetter = None
+    bestSize = None
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        upperLetter = chr(ord(letter)-32)
+        #If the letter isn't in the input, we ignore it completely
+        if letter not in inpt:
+            continue
+
+        newInpt = [x for x in inpt if x != letter and x != upperLetter]
+        testing and print("Input after removing", letter, "/", upperLetter, ":", newInpt)
+        stack = deque()
+        stack.append(newInpt[0])
+    
+        for i in range(1, len(newInpt)):
+            #If stack is empty, nothing to compare
+            if len(stack) == 0:
+                stack.append(newInpt[i])
+                continue
+        
+            cur = newInpt[i]
+            prev = stack[-1]
+        
+            #If the current letter is upper-case and the previous letter is lower-case
+            if ord(cur) <= 90 and ord(prev) >=  97 and ord(cur) == ord(prev)-32:
+                stack.pop()
+            #If the current letter is lower-case and the previous letter is upper-case
+            elif ord(cur) >= 97 and ord(prev) <= 90 and ord(cur)-32 == ord(prev):
+                stack.pop()
+            #No match, so adding it to the stack
+            else:
+                stack.append(newInpt[i])
+                
+        testing and print("\tFinal stack:", stack)
+        if bestSize is None or bestSize > len(stack):
+            bestSize = len(stack)
+            bestLetter = letter
+    
+    testing and print("Removing letter", bestLetter, "gives stack size", bestSize)
+    return bestSize
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
