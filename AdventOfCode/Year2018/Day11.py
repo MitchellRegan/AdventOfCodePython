b2c009263f72202e39541f6e2ec7c09583b01281
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-09-16 18:58:12 -0500

    Completed year 2018 day 11 part 2, though my solution is quite slow and takes about a minute to complete. I'll need to come back to it at some point to optimize the code

--- AdventOfCode/Year2018/Day11.py
+++ AdventOfCode/Year2018/Day11.py
@@ -57,20 +57,47 @@ def solution2():
         powerLevel -= 5
         return powerLevel
 
-    posPower = {} #Key is (x,y) pos, value is power level
+    #Creating the 300x300 grid of power values for every (x,y) coordinate
+    powerGrid = []
+    for r in range(1,301):
+        newRow = []
+        for c in range(1,301):
+            newRow.append(getPower(c,r,serialNum))
+        powerGrid.append(newRow)
+
+    if testing:
+        for r in range(4):
+            print(powerGrid[r][:4])
+        print()
+
     mostPower = 0
     bestPos = None
-    for x in range(1, 298):
-        for y in range(1, 298):
+    for r in range(300):
+        testing and print("Row =", r+1, "/ 300")
+        for c in range(300):
+            #testing and print("\tCol =", c+1, "/ 300")
             totalPower = 0
-            for pos in [(x,y), (x+1,y), (x+2,y), (x,y+1), (x+1,y+1), (x+2,y+1), (x,y+2), (x+1,y+2), (x+2,y+2)]:
-                if pos not in posPower.keys():
-                    posPower[pos] = getPower(pos[0], pos[1], serialNum)
-                totalPower += posPower[pos]
-            if totalPower > mostPower:
-                mostPower = totalPower
-                bestPos = (x,y)
-    return bestPos
+            for size in range(min(300-r, 300-c)):
+                #testing and print("\t\tSize =", size+1, "/", min(300-r, 300-c)+1)
+                if size == 0:
+                    totalPower = powerGrid[r][c]
+                    #testing and print("\t\t\tStarting power of 1x1 square:", totalPower)
+                else:
+                    #Getting the power from the next column to the right
+                    for nr in range(0,size):
+                        totalPower += powerGrid[r+nr][c+size]
+                        #testing and print("\t\t\tAdding value at pos", r+nr, c+size, ":", powerGrid[r+nr][c+size])
+                    
+                    #Getting the power from the next row down
+                    totalPower += sum(powerGrid[r+size][c:c+1+size])
+                    #testing and print("\t\t\tAdding sum of row:", powerGrid[r+size][c:c+1+size])
+                if totalPower > mostPower:
+                    mostPower = totalPower
+                    bestPos = (c+1,r+1,size+1)
+                #testing and print("\t\t\t==== Total power:", totalPower)
+                #if size == 3:
+                #    return
+    return str(bestPos[0]) + ',' + str(bestPos[1]) + ',' + str(bestPos[2])
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
