1b6706967b176a5b1502a5b7fcd9e826cfa839bc
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-07 10:26:29 -0500

    Completed year 2017 day 21 parts 1 and 2, and day 23 part 1 (still working on part 2)

--- AdventOfCode/Year2017/Day21.py
+++ AdventOfCode/Year2017/Day21.py
@@ -1,34 +1,125 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = {}
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            line = line.replace('\n', '').split(" => ")
+            inpt[line[0]] = line[1]
 
     return inpt
             
 
 def solution1():
     inpt = getInput()
-    
-    
+    grid = [".#.", "..#", "###"]
+        
+    def getPattern(tile_:list):
+        for i in range(8):
+            if i == 4:
+                tile_ = [x[::-1] for x in tile_]
+            key = '/'.join(tile_)
+            #testing and print("Checking for pattern", key)
+            #for x in tile_:
+            #    print(x)
+            if key in inpt.keys():
+                testing and print("\tFound", key, "==>", inpt[key])
+                return inpt[key]
+            tile_ = [''.join(x) for x in list(zip(*tile_[::-1]))]
 
-    return
+        
+    for t in range(5):
+        newGrid = []
+        if len(grid[0]) % 2 == 0:
+            for r in range(0, len(grid), 2):
+                for c in range(0, len(grid[0]), 2):
+                    ptrn = getPattern([grid[r][c:c+2], grid[r+1][c:c+2]])
+                    ptrn = ptrn.split('/')
+                    if c == 0:
+                        for x in ptrn:
+                            newGrid.append(x)
+                    else:
+                        i = len(newGrid) - len(ptrn)
+                        for x in range(len(ptrn)):
+                            newGrid[i+x] = newGrid[i+x] + ptrn[x]
+        elif len(grid[0]) % 3 == 0:
+            for r in range(0, len(grid), 3):
+                for c in range(0, len(grid[0]), 3):
+                    ptrn = getPattern([grid[r][c:c+3], grid[r+1][c:c+3], grid[r+2][c:c+3]])
+                    ptrn = ptrn.split('/')
+                    if c == 0:
+                        for x in ptrn:
+                            newGrid.append(x)
+                    else:
+                        i = len(newGrid) - len(ptrn)
+                        for x in range(len(ptrn)):
+                            newGrid[i+x] = newGrid[i+x] + ptrn[x]
+        grid = newGrid
+        if testing:
+            print("New grid after t =", t)
+            for x in grid:
+                print(x)
+                
+    ans = 0
+    for row in grid:
+        ans += row.count('#')
+    return ans
 
 
 def solution2():
     inpt = getInput()
-    
-    return
+    grid = [".#.", "..#", "###"]
+        
+    def getPattern(tile_:list):
+        for i in range(8):
+            if i == 4:
+                tile_ = [x[::-1] for x in tile_]
+            key = '/'.join(tile_)
+            if key in inpt.keys():
+                return inpt[key]
+            tile_ = [''.join(x) for x in list(zip(*tile_[::-1]))]
+
+        
+    for t in range(18):
+        newGrid = []
+        if len(grid[0]) % 2 == 0:
+            for r in range(0, len(grid), 2):
+                for c in range(0, len(grid[0]), 2):
+                    ptrn = getPattern([grid[r][c:c+2], grid[r+1][c:c+2]])
+                    ptrn = ptrn.split('/')
+                    if c == 0:
+                        for x in ptrn:
+                            newGrid.append(x)
+                    else:
+                        i = len(newGrid) - len(ptrn)
+                        for x in range(len(ptrn)):
+                            newGrid[i+x] = newGrid[i+x] + ptrn[x]
+        elif len(grid[0]) % 3 == 0:
+            for r in range(0, len(grid), 3):
+                for c in range(0, len(grid[0]), 3):
+                    ptrn = getPattern([grid[r][c:c+3], grid[r+1][c:c+3], grid[r+2][c:c+3]])
+                    ptrn = ptrn.split('/')
+                    if c == 0:
+                        for x in ptrn:
+                            newGrid.append(x)
+                    else:
+                        i = len(newGrid) - len(ptrn)
+                        for x in range(len(ptrn)):
+                            newGrid[i+x] = newGrid[i+x] + ptrn[x]
+        grid = newGrid
+                
+    ans = 0
+    for row in grid:
+        ans += row.count('#')
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
--- AdventOfCode/Year2017/Day23.py
+++ AdventOfCode/Year2017/Day23.py
@@ -1,34 +1,119 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
     inpt = []
+    regs = {}
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
-
-    return inpt
+            line = line.replace('\n', '').split(' ')
+            
+            if line[1].isdigit():
+                line[1] = int(line[1])
+            elif line[1] not in regs.keys():
+                regs[line[1]] = 0
+                
+            if len(line) > 2:
+                if line[2].isdigit() or line[2][0] == '-':
+                    line[2] = int(line[2])
+                elif line[2] not in regs.keys():
+                    regs[line[2]] = 0
+            inpt.append(line)
+    return inpt, regs
             
 
 def solution1():
-    inpt = getInput()
+    inpt, regs = getInput()
     
-    
+    if True:
+        print("Registers:")
+        for r in regs.keys():
+            print("\t", r, ":", regs[r])
+        print("Input Instructions:")
+        for i in inpt:
+            print("\t", i)
 
-    return
+    iptr = 0
+    ans = 0
+    while iptr > -1 and iptr < len(inpt):
+        i = inpt[iptr]
+        val2 = 0
+        if len(i) > 2:
+            val2 = i[2]
+            if type(val2) is not int:
+                val2 = regs[val2]
+                
+        if i[0] == "set":
+            regs[i[1]] = val2
+            iptr += 1
+        elif i[0] == "sub":
+            regs[i[1]] -= val2
+            iptr += 1
+        elif i[0] == "mul":
+            regs[i[1]] *= val2
+            ans += 1
+            iptr += 1
+        elif i[0] == "jnz":
+            val1 = i[1]
+            if type(val1) is not int:
+                val1 = regs[val1]
+            if val1 != 0:
+                iptr += val2
+            else:
+                iptr += 1
+
+    return ans
 
 
 def solution2():
-    inpt = getInput()
-    
-    return
+    inpt, regs = getInput()
+    regs['a'] = 1
+
+    iptr = 0
+    t = 0
+    while iptr > -1 and iptr < len(inpt):
+        i = inpt[iptr]
+        print("iptr =", iptr, ":", i)
+        if type(i[1]) is not int:
+            print("\t", i[1], ":", regs[i[1]])
+        if type(i[2]) is not int:
+            print("\t", i[2], ":", regs[i[2]])
+        val2 = 0
+        if len(i) > 2:
+            val2 = i[2]
+            if type(val2) is not int:
+                val2 = regs[val2]
+                
+        if i[0] == "set":
+            regs[i[1]] = val2
+            iptr += 1
+        elif i[0] == "sub":
+            regs[i[1]] -= val2
+            iptr += 1
+        elif i[0] == "mul":
+            regs[i[1]] *= val2
+            iptr += 1
+        elif i[0] == "jnz":
+            val1 = i[1]
+            if type(val1) is not int:
+                val1 = regs[val1]
+            if val1 != 0:
+                iptr += val2
+            else:
+                iptr += 1
+                
+        if i[1] == 'h':
+            print("t =", t, "    h:", regs['h'])
+        t += 1
+
+    return regs['h']
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
