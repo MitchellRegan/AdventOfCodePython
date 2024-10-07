ec5e54478757f09ee6ccbecf69e8b2957fc94bd8
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-07 07:14:17 -0500

    Completed year 2017 day 15 parts 1 and 2

--- AdventOfCode/Year2017/Day15.py
+++ AdventOfCode/Year2017/Day15.py
@@ -1,34 +1,68 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    genA = None
+    genB = None
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            line = line.replace('\n', '').split(' ')
+            if genA is None:
+                genA = int(line[-1])
+            else:
+                genB = int(line[-1])
 
-    return inpt
+    return genA, genB
             
 
 def solution1():
-    inpt = getInput()
+    genA, genB = getInput()
+    numLoops = 40000000
     
-    
+    ans = 0
+    for i in range(numLoops):
+        if testing and i % 1000000 == 0:
+            print("Loop", i, "/ 40,000,000")
+        genA = (genA * 16807) % 2147483647
+        genB = (genB * 48271) % 2147483647
+        #testing and print("Loop", i, "    GenA:", genA, "    GenB:", genB)
+        binA = f'{genA:08b}'[-16:]
+        binB = f'{genB:08b}'[-16:]
+        #testing and print(binA)
+        #testing and print(binB, "\n")
+        if binA == binB:
+            ans += 1
 
-    return
+    return ans
 
 
 def solution2():
-    inpt = getInput()
-    
-    return
+    genA, genB = getInput()
+    numLoops = 5000000
+
+    ans = 0
+    for i in range(numLoops):
+        genA = (genA * 16807) % 2147483647
+        while genA % 4 != 0:
+            genA = (genA * 16807) % 2147483647
+        genB = (genB * 48271) % 2147483647
+        while genB % 8 != 0:
+            genB = (genB * 48271) % 2147483647
+            
+        binA = f'{genA:08b}'[-16:]
+        binB = f'{genB:08b}'[-16:]
+        
+        if binA == binB:
+            ans += 1
+
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
