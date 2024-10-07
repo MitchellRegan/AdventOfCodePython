8c64b0a29dbb8b19b8fe70bd8d6b723b37a041fb
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-07 06:41:59 -0500

    Completed year 2017 day 14 parts 1 and 2, and day 25 part 1

--- AdventOfCode/Year2017/Day14.py
+++ AdventOfCode/Year2017/Day14.py
@@ -1,10 +1,10 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
@@ -12,23 +12,105 @@ def getInput():
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            inpt = line.replace('\n', '')
 
     return inpt
             
 
+def knotHash(inpt):
+    inpt = [ord(c) for c in inpt]
+    inpt.extend([17, 31, 73, 47, 23])
+    
+    currIndex = 0
+    skipSize = 0
+    numberRope = [x for x in range(256)]
+        
+    #Looping 64 times
+    for loop in range(64):
+        for ropeLen in inpt:
+            #If the length of rope is 1, there's no point in reversing the numbers because it's the same
+            if ropeLen != 1:
+                revNums = []
+                if currIndex + ropeLen >= len(numberRope):
+                    revNums = numberRope[currIndex:]
+                    revNums.extend(numberRope[:currIndex+ropeLen-len(numberRope)])
+                else:
+                    revNums = numberRope[currIndex:currIndex+ropeLen]
+                    
+                revNums.reverse()
+        
+                for i in range(ropeLen):
+                    if currIndex + i >= len(numberRope):
+                        numberRope[currIndex+i-len(numberRope)] = revNums[i]
+                    else:
+                        numberRope[currIndex+i] = revNums[i]
+                
+            currIndex += ropeLen + skipSize
+            while currIndex >= len(numberRope):
+                currIndex -= len(numberRope)
+            skipSize += 1
+            
+    hashStr = ""
+    curVal = None
+    for i in range(len(numberRope)):
+        if i % 16 == 0:
+            curVal = numberRope[i]
+        else:
+            #Performing a bitwise XOR
+            curVal = curVal ^ numberRope[i]
+            if i % 16 == 15:
+                if curVal < 16:
+                    hashStr = hashStr + '0' + hex(curVal)[2:]
+                else:
+                    hashStr = hashStr + hex(curVal)[2:]
+    return hashStr
+
+
 def solution1():
     inpt = getInput()
     
-    
+    print(inpt)
+    ans = 0
+    for row in range(0, 128):
+        inputString = inpt + '-' + str(row)
+        outputString = knotHash(inputString)
+        binString = bin(int(outputString, 16))[2:].zfill(128)
+        
+        if testing and row < 8:
+            print(binString[:8].replace('1','#').replace('0','.'), "    ", outputString[:2], "     Hex size:", len(outputString))
+        ans += binString.count('1')
 
-    return
+    return ans
 
 
 def solution2():
     inpt = getInput()
+    grid = []
     
-    return
+    for row in range(0, 128):
+        inputString = inpt + '-' + str(row)
+        outputString = knotHash(inputString)
+        binString = bin(int(outputString, 16))[2:].zfill(128)
+        grid.append([c for c in binString])
+        
+    #Iterating through every (row,col) position to look for a non-0 value to begin a BFS search for groups
+    numGroups = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == '1':
+                numGroups += 1
+                seen = {(r,c):True}
+                q = [(r,c)]
+                while len(q) > 0:
+                    hr,hc = q.pop(0)
+                    grid[hr][hc] = '0'
+                    for adj in [(hr+1,hc), (hr-1,hc), (hr,hc+1), (hr,hc-1)]:
+                        if adj not in seen.keys() and adj[0] > -1 and adj[0] < 128 and adj[1] > -1 and adj[1] < 128:
+                            if grid[adj[0]][adj[1]] == '1':
+                                q.append(adj)
+                            seen[adj] = True
+
+    return numGroups
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
--- AdventOfCode/Year2017/Day25.py
+++ AdventOfCode/Year2017/Day25.py
@@ -1,28 +1,103 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
+
+
+class State:
+    def __init__(self, id_:str):
+        self.id = id_
+        self.compVal1 = None
+        self.compVal2 = None
+        self.writeVal1 = None
+        self.writeVal2 = None
+        self.moveVal1 = None
+        self.moveVal2 = None
+        self.continueState1 = None
+        self.continueState2 = None
+        
+    def __str__(self):
+        return f"State {self.id}\n\tIf current value = {self.compVal1} write {self.writeVal1} move {self.moveVal1} go to state {self.continueState1}\n\tIf current value = {self.compVal2} write {self.writeVal2} move {self.moveVal2} go to state {self.continueState2}"
 
 
 def getInput():
-    inpt = []
-
+    inpt = {} #Value = state ID char, Value = State class
+    beginState = None
+    steps = None
+    
+    newState = None
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            line = line.replace('\n', '').split(' ')
+            if beginState is None:
+                beginState = line[-1][0]
+            elif steps is None:
+                steps = int(line[-2])
+            elif len(line) == 1 and newState is not None:
+                inpt[newState.id] = newState
+                newState = None
+            elif len(line) > 1:
+                if line[0] == "In": #Starting a new state
+                    newState = State(line[2][0])
+                elif line[2] == "If": #Getting the comparisson values
+                    if newState.compVal1 is None:
+                        newState.compVal1 = int(line[-1].replace(':', ''))
+                    else:
+                        newState.compVal2 = int(line[-1].replace(':', ''))
+                elif line[5] == "Write": #Getting the values to write
+                    if newState.writeVal1 is None:
+                        newState.writeVal1 = int(line[-1].replace('.', ''))
+                    else:
+                        newState.writeVal2 = int(line[-1].replace('.', ''))
+                elif line[5] == "Move": #Finding the move value
+                    moveVal = 1
+                    if line[-1] == "left.":
+                        moveVal = -1
+                    if newState.moveVal1 is None:
+                        newState.moveVal1 = moveVal
+                    else:
+                        newState.moveVal2 = moveVal
+                elif line[5] == "Continue": #Finding the next state
+                    if newState.continueState1 is None:
+                        newState.continueState1 = line[-1][0]
+                    else:
+                        newState.continueState2 = line[-1][0]
 
-    return inpt
+    inpt[newState.id] = newState
+    return inpt, beginState, steps
             
 
 def solution1():
-    inpt = getInput()
+    states, curState, steps = getInput()
+    posVals = {} #Key = index int, Value = binary value at the index. Defaults to 0
     
-    
+    if testing:
+        print("Current State:", curState, "    Number of steps:", steps)
+        for s in states.keys():
+            print(states[s])
 
-    return
+    i = 0
+    for step in range(steps):
+        if i not in posVals.keys():
+            posVals[i] = 0
+        if posVals[i] == states[curState].compVal1:
+            posVals[i] = states[curState].writeVal1
+            i += states[curState].moveVal1
+            curState = states[curState].continueState1
+        else:
+            posVals[i] = states[curState].writeVal2
+            i += states[curState].moveVal2
+            curState = states[curState].continueState2
+
+    ans = 0
+    testing and print(posVals)
+    for p in posVals.keys():
+        if posVals[p] == 1:
+            ans += 1
+    return ans
 
 
 def solution2():
