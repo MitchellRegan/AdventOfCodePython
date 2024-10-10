a520e6c8197ba00835e1ecc650f07a5cd164a1d7
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-10 18:02:29 -0500

    Completed year 2016 days 15 and 16, parts 1 and 2

--- AdventOfCode/Year2016/Day15.py
+++ AdventOfCode/Year2016/Day15.py
@@ -1,10 +1,10 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
@@ -12,7 +12,8 @@ def getInput():
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            line = line.replace('\n', '').split(' ')
+            inpt.append([int(line[1][1:]), int(line[3]), int(line[-1][:-1])])
 
     return inpt
             
@@ -20,14 +21,35 @@ def getInput():
 def solution1():
     inpt = getInput()
     
-    
+    t = 0
+    while True:
+        valid = True
+        for i in inpt:
+            if (t + i[0] + i[2]) % i[1] != 0:
+                valid = False
+                break
+        if valid:
+            return t
+        t += 1
 
     return
 
 
 def solution2():
     inpt = getInput()
+    inpt.append([inpt[-1][0]+1, 11, 0])
     
+    t = 0
+    while True:
+        valid = True
+        for i in inpt:
+            if (t + i[0] + i[2]) % i[1] != 0:
+                valid = False
+                break
+        if valid:
+            return t
+        t += 1
+
     return
 
 
--- AdventOfCode/Year2016/Day16.py
+++ AdventOfCode/Year2016/Day16.py
@@ -1,10 +1,10 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
@@ -12,23 +12,61 @@ def getInput():
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            inpt = line.replace('\n', '')
 
     return inpt
             
 
 def solution1():
     inpt = getInput()
+    targetLen = 272
+    if testing:
+        targetLen = 20
+        
+    testing and print(inpt)
+    while len(inpt) < targetLen:
+        newInpt = inpt[::-1]
+        newInpt = newInpt.replace('0',' ').replace('1','0').replace(' ','1')
+        inpt = inpt + '0' + newInpt
+        testing and print(inpt, "    New Inpt:", newInpt)
+    inpt = inpt[:targetLen]
+    testing and print(inpt, "    Trimmed to length", targetLen)
     
-    
+    #Performing checksum until input string length is odd
+    while len(inpt) % 2 == 0:
+        checkSum = ""
+        for i in range(0, len(inpt), 2):
+            if inpt[i] == inpt[i+1]:
+                checkSum = checkSum + '1'
+            else:
+                checkSum = checkSum + '0'
+        testing and print(inpt, " --> ", checkSum)
+        inpt = checkSum
 
-    return
+    return inpt
 
 
 def solution2():
     inpt = getInput()
+    targetLen = 35651584
+        
+    while len(inpt) < targetLen:
+        newInpt = inpt[::-1]
+        newInpt = newInpt.replace('0',' ').replace('1','0').replace(' ','1')
+        inpt = inpt + '0' + newInpt
+    inpt = inpt[:targetLen]
     
-    return
+    #Performing checksum until input string length is odd
+    while len(inpt) % 2 == 0:
+        checkSum = ""
+        for i in range(0, len(inpt), 2):
+            if inpt[i] == inpt[i+1]:
+                checkSum = checkSum + '1'
+            else:
+                checkSum = checkSum + '0'
+        inpt = checkSum
+
+    return inpt
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
