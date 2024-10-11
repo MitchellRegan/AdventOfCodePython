83dee7181875c99171a8ab4a73f99913a4b9faf8
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-11 15:55:53 -0500

    Completed year 2016 day 5 parts 1 and 2

--- AdventOfCode/Year2016/Day5.py
+++ AdventOfCode/Year2016/Day5.py
@@ -1,6 +1,7 @@
+import hashlib
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
@@ -20,15 +21,38 @@ def getInput():
 def solution1():
     inpt = getInput()
     
-    print("Input:", inpt)
-    
-
-    return
+    ans = ''
+    i = 0
+    while len(ans) < 8:
+        s = inpt + str(i)
+        hs = hashlib.md5(s.encode()).hexdigest()
+        if hs[:5] == "00000":
+            ans = ans + hs[5]
+            testing and print("i =", i, "String:", s, "    Hash:", hs, "    Pw:", ans)
+        i += 1
+        
+    return ans
 
 
 def solution2():
     inpt = getInput()
     
+    ans = [None] * 8
+    i = 0
+    while True:
+        s = inpt + str(i)
+        hs = hashlib.md5(s.encode()).hexdigest()
+        if hs[0] == '0' and hs[1] == '0' and hs[2] == '0' and hs[3] == '0' and hs[4] == '0':
+            testing and print("Possible hash:", hs)
+            if hs[5] in "01234567":
+                ind = int(hs[5])
+                if ans[ind] is None:
+                    ans[ind] = hs[6]
+                    testing and print("i =", i, "String:", s, "    Hash:", hs, "    Pw:", ans)
+                    if None not in ans:
+                        return ''.join(ans)
+        i += 1
+        
     return
 
 
