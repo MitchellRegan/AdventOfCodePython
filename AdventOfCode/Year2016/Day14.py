ae71351b195a6826395e8d925910d5d4c8f50de3
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-11 17:46:17 -0500

    Completed year 2016 day 14 part 1, and day 17 parts 1 and 2

--- AdventOfCode/Year2016/Day14.py
+++ AdventOfCode/Year2016/Day14.py
@@ -1,18 +1,19 @@
+import hashlib
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
 testing = 1
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = ""
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            inpt = line.replace('\n', '')
 
     return inpt
             
@@ -20,16 +21,87 @@ def getInput():
 def solution1():
     inpt = getInput()
     
+    def hasTriple(s_:str)->bool:
+        for i in range(len(s_)-2):
+            if s_[i] == s_[i+1] and s_[i] == s_[i+2]:
+                return s_[i]
+        return
     
-
-    return
+    ans = []
+    i = 0
+    q = [] #Every value is (String of the same char to look for, index in which to pop if not found)
+    while len(ans) < 64:
+        if len(q) > 0 and i > q[0][1]:
+            #testing and print(q[0], "not found by i =", i)
+            q.pop(0)
+        s = inpt + str(i)
+        hs = hashlib.md5(s.encode()).hexdigest()
+            
+        #Looking for the strings of 5 repeating characters to find any matches
+        removeInd = []
+        for r in range(len(q)):
+            if q[r][0] in hs:
+                ans.append(q[r][1]-1000)
+                #testing and print("i =", i, "    Hash:", hs, "    Found x5:", q[r][0])
+                removeInd.insert(0, r)
+        for r in removeInd:
+            q.pop(r)
+            
+        #Looking for strings of 3 repeating characters
+        trip = hasTriple(hs)
+        if trip:
+            q.append((''.join([trip]*5), i+1000))
+            #testing and print("i =", i, "    Hash:", hs, "    Found x3", trip)
+            
+        i += 1
+        
+    return ans[-1]
 
 
 def solution2():
     inpt = getInput()
     
-    return
+    def hasTriple(s_:str)->bool:
+        for i in range(len(s_)-2):
+            if s_[i] == s_[i+1] and s_[i] == s_[i+2]:
+                return s_[i]
+        return
+    
+    ans = []
+    i = 0
+    q = [] #Every value is (String of the same char to look for, index in which to pop if not found)
+    while len(ans) < 64:
+        if len(q) > 0 and i > q[0][1]:
+            q.pop(0)
+            
+        s = inpt + str(i)
+        hs = hashlib.md5(s.encode()).hexdigest()
+        for salt in range(2016):
+            hs = hashlib.md5(hs.encode()).hexdigest()
+            
+        #Looking for the strings of 5 repeating characters to find any matches
+        removeInd = []
+        for r in range(len(q)):
+            if q[r][0] in hs:
+                ans.append(q[r][1]-1000)
+                print("i =", i, "    Hash:", hs, "    Found x5:", q[r][0], "for index", ans[-1], "    Key#", len(ans))
+                removeInd.insert(0, r)
+        for r in removeInd:
+            q.pop(r)
+            
+        #Looking for strings of 3 repeating characters
+        trip = hasTriple(hs)
+        if trip:
+            q.append((''.join([trip]*5), i+1000))
+            #testing and print("i =", i, "    Hash:", hs, "    Found x3", trip)
+            
+        i += 1
+        
+    return ans[63]
 
 
-print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
+#print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 2:", solution2())
+#20219 too high
+#20119 too high
+#20153 too high
--- AdventOfCode/Year2016/Day17.py
+++ AdventOfCode/Year2016/Day17.py
@@ -1,18 +1,19 @@
+import hashlib
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = ""
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            inpt = line.replace('\n', '')
 
     return inpt
             
@@ -20,7 +21,21 @@ def getInput():
 def solution1():
     inpt = getInput()
     
-    
+    q = [(inpt,0,0)] #Each value is (string to hash, row pos, column pos)
+    while True:
+        s,r,c = q.pop(0)
+        if r == 3 and c == 3:
+            return s[len(inpt):]
+        hs = hashlib.md5(s.encode()).hexdigest()
+        
+        if r > 0 and hs[0] in "bcdef": #UP
+            q.append((s+"U", r-1, c))
+        if r < 3 and hs[1] in "bcdef": #DOWN
+            q.append((s+"D", r+1, c))
+        if c > 0 and hs[2] in "bcdef": #LEFT
+            q.append((s+"L", r, c-1))
+        if c < 3 and hs[3] in "bcdef": #RIGHT
+            q.append((s+"R", r, c+1))
 
     return
 
@@ -28,7 +43,25 @@ def solution1():
 def solution2():
     inpt = getInput()
     
-    return
+    ans = 0
+    q = [(inpt,0,0)] #Each value is (string to hash, row pos, column pos)
+    while len(q) > 0:
+        s,r,c = q.pop(0)
+        if r == 3 and c == 3:
+            ans = max(ans, len(s)-len(inpt))
+        else:
+            hs = hashlib.md5(s.encode()).hexdigest()
+        
+            if r > 0 and hs[0] in "bcdef": #UP
+                q.append((s+"U", r-1, c))
+            if r < 3 and hs[1] in "bcdef": #DOWN
+                q.append((s+"D", r+1, c))
+            if c > 0 and hs[2] in "bcdef": #LEFT
+                q.append((s+"L", r, c-1))
+            if c < 3 and hs[3] in "bcdef": #RIGHT
+                q.append((s+"R", r, c+1))
+
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
