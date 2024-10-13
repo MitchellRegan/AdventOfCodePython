0b85edf87c1f7b011c4c26086ece4f37f1943b0c
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-13 02:16:01 -0500

    Completed year 2015 day 10 both parts, after horribly over-complicating the solution to part 2

--- AdventOfCode/Year2015/Day10.py
+++ AdventOfCode/Year2015/Day10.py
@@ -1,34 +1,73 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = ""
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            inpt = line.replace('\n', '')
 
     return inpt
             
 
 def solution1():
     inpt = getInput()
+    testing and print("Input string:", inpt)
     
-    
+    for x in range(40):
+        nextInpt = ""
+        i = 0
+        while i < len(inpt):
+            sameDigit = 1
+            j = i+1
+            while j < len(inpt):
+                if inpt[j] == inpt[i]:
+                    sameDigit += 1
+                    j += 1
+                else:
+                    break
+            testing and print("\t", inpt[i:j], "becomes", j-i, inpt[i])
+            nextInpt = nextInpt + str(j-i) + inpt[i]
+            i = j
+        inpt = nextInpt
 
-    return
+    return len(inpt)
+
+
+from collections import deque
+def nextLookAndSaySeq(lass_:str):
+    '''Takes a Look-And-Say-Sequence (LASS) string and returns the next sequence string.'''
+    nextS = deque()
+    i = 0
+    while i < len(lass_):
+        sameDigit = 1
+        j = i+1
+        while j < len(lass_):
+            if lass_[j] == lass_[i]:
+                sameDigit += 1
+                j += 1
+            else:
+                break
+        nextS.append(str(j-i))
+        nextS.append(lass_[i])
+        i = j
+    return ''.join(nextS)
 
 
 def solution2():
     inpt = getInput()
     
-    return
+    for x in range(50):
+        inpt = nextLookAndSaySeq(inpt)
+
+    return len(inpt)
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
