231147eee05d01d56460f8088a60acdf4108fc9c
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-14 15:47:27 -0500

    Completed year 2015 day 12 both parts

--- AdventOfCode/Year2015/Day12.py
+++ AdventOfCode/Year2015/Day12.py
@@ -1,7 +1,7 @@
 import json
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
@@ -21,17 +21,77 @@ def getInput():
 def solution1():
     inpt = getInput()
     
+    def checkList(cl_:list)->int:
+        '''Recursive method that looks through every element in the given list and returns the sum of all ints.'''
+        s = 0
+        for i in cl_:
+            if type(i) is int:
+                s += i
+            elif type(i) is list:
+                s += checkList(i)
+            elif type(i) is dict:
+                s += checkDict(i)
+        testing and print("++++ List", cl_, "sum:", s)
+        return s
+    
+    def checkDict(cd_:dict)->int:
+        '''Recursive method that looks through every key/value pair in the given dictionary and returns the sum of all ints.'''
+        s = 0
+        for k in cd_.keys():
+            if type(k) is int:
+                s += k
+            if type(cd_[k]) is int:
+                s += cd_[k]
+            elif type(cd_[k]) is dict:
+                s += checkDict(cd_[k])
+            elif type(cd_[k]) is list:
+                s += checkList(cd_[k])
+        testing and print("==== Dict", cd_, "sum:", s)
+        return s
+    
     js = json.loads(inpt)
-    for k in js.keys():
-        print("\t", k, ":", type(js[k]))
 
-    return
+    return checkDict(js)
 
 
 def solution2():
     inpt = getInput()
     
-    return
+    def checkList(cl_:list)->int:
+        '''Recursive method that looks through every element in the given list and returns the sum of all ints.'''
+        s = 0
+        for i in cl_:
+            if type(i) is int:
+                s += i
+            elif type(i) is list:
+                s += checkList(i)
+            elif type(i) is dict:
+                s += checkDict(i)
+        testing and print("++++ List", cl_, "sum:", s)
+        return s
+    
+    def checkDict(cd_:dict)->int:
+        '''Recursive method that looks through every key/value pair in the given dictionary and returns the sum of all ints.'''
+        s = 0
+        for k in cd_.keys():
+            if type(k) is int:
+                s += k
+                
+            if type(cd_[k]) is int:
+                s += cd_[k]
+            elif type(cd_[k]) is dict:
+                s += checkDict(cd_[k])
+            elif type(cd_[k]) is list:
+                s += checkList(cd_[k])
+            elif type(cd_[k]) is str and cd_[k] == "red":
+                s = 0
+                break
+        testing and print("==== Dict", cd_, "sum:", s)
+        return s
+    
+    js = json.loads(inpt)
+
+    return checkDict(js)
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
