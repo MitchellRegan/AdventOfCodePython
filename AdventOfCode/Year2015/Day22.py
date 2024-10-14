ef511551585b04c1fa89a3833bb9028be9139458
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-14 13:33:04 -0500

    Completed year 2015 day 23 both parts, and started on day 22 but I'll probably come back to it later

--- AdventOfCode/Year2015/Day22.py
+++ AdventOfCode/Year2015/Day22.py
@@ -4,29 +4,84 @@ testing = 1
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
+
+class GameChar:
+    def __init__(self, name_:str, hp_:int, dmg_:int, arm_:int):
+        '''Initializes a game character with health, damage (attack power), and armor values.'''
+        self.name = name_
+        self.hp = hp_
+        self.damage = dmg_
+        
+    def takeDamage(self, incommingDmg_:int)->bool:
+        '''Applies incomming damage to this character. Returns True if still alive, and False if dead.'''
+        totalDmg = incommingDmg_ - self.armor
+        if totalDmg < 1:
+            totalDmg = 1
+        self.hp -= totalDmg
+        testing and print("\t", self.name, "attacked for", incommingDmg_, "-", self.armor, "=", totalDmg, "    HP:", self.hp)
+            
+        return (self.hp > 0)
+ 
 
 def getInput():
-    inpt = []
+    player = [50, 500]
+    boss = [0,0]
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            line = line.replace('\n', '').split(' ')
+            if boss[0] == 0:
+                boss[0] = int(line[-1])
+            else:
+                boss[1] = int(line[-1])
 
-    return inpt
+    return player, boss
             
 
 def solution1():
-    inpt = getInput()
+    ps, bs = getInput()
+    if testing:
+        ps = [10, 250]
+        
+    spells = [("Magic Missile", 53), ("Drain", 73), ("Shield", 113), ("Poison", 173), ("Recharge", 229)]
     
-    
+    print("Player HP:", ps[0], "    Mana:  ", ps[1], "\nBoss   HP:", bs[0], "    Damage:", bs[1])
+    def runCombat(hp_:int, mana_:int, manaUsed_:int, bossHp_:int, bossDmg_:int, shieldTime_:int=0, poisonTime_:int=0, rechargeTime_:int=0)->int:
+        '''Recursive method to run each round of combat, simulating the results of every different spell choice.'''
+        
+        #Taking care of effects first
+        if shieldTime_ > 0:
+            shieldTime_ -= 1
+        if poisonTime_ > 0:
+            poisonTime_ -= 1
+            bossHp_ -= 3
+        if rechargeTime_ > 0:
+            rechargeTime_ -= 1
+            mana_ += 101
+            
+        #If the player can't afford even the most inexpensive spell, they lose
+        if mana_ < spells[0][1]:
+            return manaUsed_
+        #Iterating through every spell option the player can cast
+        for spell in spells:
+            if spell[1] <= mana_:
+                if spell[0] == "Magic Missile":
+                    
+                elif spell[0] == "Drain":
+                    
+                elif spell[0] == "Shield":
+                    
+                elif spell[0] == "Poison":
+                    
+                elif spell[0] == "Recharge":
 
-    return
+    return runCombat()
 
 
 def solution2():
-    inpt = getInput()
+    playerStats, bossStats = getInput()
     
     return
 
--- AdventOfCode/Year2015/Day23.py
+++ AdventOfCode/Year2015/Day23.py
@@ -1,34 +1,92 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
     inpt = []
+    regs = {}
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            line = line.replace('\n', '').replace(',', '').replace('+', '').split(' ')
+            if line[1].isdigit() or line[1][0] == '-':
+                line[1] = int(line[1])
+            if len(line) == 3 and (line[2].isdigit() or line[2][0] == '-'):
+                line[2] = int(line[2])
+            inpt.append(line)
 
     return inpt
             
 
 def solution1():
     inpt = getInput()
+    regs = {'a':0, 'b':0}
     
-    
+    i = 0
+    while i > -1 and i < len(inpt):
+        testing and print("i =", i, "    ", inpt[i])
+        if inpt[i][0] == "hlf":
+            regs[inpt[i][1]] = regs[inpt[i][1]] // 2
+            i += 1
+        elif inpt[i][0] == "tpl":
+            regs[inpt[i][1]] = 3 * regs[inpt[i][1]]
+            i += 1
+        elif inpt[i][0] == "inc":
+            regs[inpt[i][1]] += 1
+            i += 1
+        elif inpt[i][0] == "jmp":
+            i += inpt[i][1]
+        elif inpt[i][0] == "jie":
+            if regs[inpt[i][1]] % 2 == 0:
+                i += inpt[i][2]
+            else:
+                i += 1
+        elif inpt[i][0] == "jio":
+            if regs[inpt[i][1]] == 1:
+                i += inpt[i][2]
+            else:
+                i += 1
+        testing and print("\tNew i =", i, "    Registers:", regs)
 
-    return
+    return regs['b']
 
 
 def solution2():
     inpt = getInput()
+    regs = {'a':1, 'b':0}
     
-    return
+    i = 0
+    while i > -1 and i < len(inpt):
+        testing and print("i =", i, "    ", inpt[i])
+        if inpt[i][0] == "hlf":
+            regs[inpt[i][1]] = regs[inpt[i][1]] // 2
+            i += 1
+        elif inpt[i][0] == "tpl":
+            regs[inpt[i][1]] = 3 * regs[inpt[i][1]]
+            i += 1
+        elif inpt[i][0] == "inc":
+            regs[inpt[i][1]] += 1
+            i += 1
+        elif inpt[i][0] == "jmp":
+            i += inpt[i][1]
+        elif inpt[i][0] == "jie":
+            if regs[inpt[i][1]] % 2 == 0:
+                i += inpt[i][2]
+            else:
+                i += 1
+        elif inpt[i][0] == "jio":
+            if regs[inpt[i][1]] == 1:
+                i += inpt[i][2]
+            else:
+                i += 1
+        testing and print("\tNew i =", i, "    Registers:", regs)
+
+    return regs['b']
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
