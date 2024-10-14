3a1ca87ed6f880de01bd664b14c6606e92df7416
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-10-13 21:16:53 -0500

    Completed year 2015 day 20 both parts

--- AdventOfCode/Year2015/Day19.py
+++ AdventOfCode/Year2015/Day19.py
@@ -1,26 +1,40 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = ""
+    molMap = {}
 
     with open(inFile, 'r') as f:
+        molecules = True
         for line in f:
             line = line.replace('\n', '')
+            if len(line) == 0:
+                molecules = False
+            elif molecules:
+                line = line.split(' ')
+                if line[0] not in molMap.keys():
+                    molMap[line[0]] = []
+                molMap[line[0]].append(line[2])
+            else:
+                inpt = line
 
-    return inpt
+    return inpt, molMap
             
 
 def solution1():
-    inpt = getInput()
+    inpt, molMap = getInput()
     
-    
+    if True:
+        print("Input string:", inpt, "\nMolecules:")
+        for k in molMap.keys():
+            print("\t", k, ":", molMap[k])
 
     return
 
--- AdventOfCode/Year2015/Day20.py
+++ AdventOfCode/Year2015/Day20.py
@@ -1,18 +1,18 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
-    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_real.txt"
+    inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
 
 
 def getInput():
-    inpt = []
+    inpt = None
 
     with open(inFile, 'r') as f:
         for line in f:
-            line = line.replace('\n', '')
+            inpt = int(line.replace('\n', ''))
 
     return inpt
             
@@ -20,14 +20,56 @@ def getInput():
 def solution1():
     inpt = getInput()
     
-    
+    i = 1
+    while True:
+        #testing and print("House", i)
+        #Getting all divisors of the current value of i
+        #divs = []
+        score = 10 + (10 * i)
+        #testing and print("\tSquare Root:", int(i**0.5))
+        for d in range(2, int(i**0.5)+1):
+            if i % d == 0:
+                #divs.append(d)
+                score += (10 * d)
+                if i//d != d:
+                    #divs.append(i//d)
+                    score += (10 * (i//d))
+        #testing and print("\tScore:", score)
+        if score >= inpt:
+            return i
+        i += 1
 
     return
 
 
 def solution2():
     inpt = getInput()
+    seen = {}#Key = int for divisor value, Value = number of times the divisor has been seen
     
+    i = 1
+    while True:
+        #Getting all divisors of the current value of i
+        score = 0
+        for d in range(1, int(i**0.5)+1):
+            if i % d == 0:
+                if d not in seen.keys():
+                    seen[d] = 0
+                seen[d] += 1
+                if seen[d] <= 50:
+                    score += (11 * d)
+                    
+                o = i//d
+                if o != d:
+                    if o not in seen.keys():
+                        seen[o] = 0
+                    seen[o] += 1
+                    if seen[o] <= 50:
+                        score += (11 * o)
+                        
+        if score >= inpt:
+            return i
+        i += 1
+
     return
 
 
