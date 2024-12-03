bfdf4edfa4cb770aee3c2f2d0151ffa33ea03922
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-03 00:20:07 -0600

    Completed year 2024 day 3 both parts

--- AdventOfCode/Year2024/Day3.py
+++ AdventOfCode/Year2024/Day3.py
@@ -8,11 +8,12 @@ else:
 
 
 def getInput():
-    inpt = ""
+    inpt = []
 
     with open(inFile, 'r') as f:
         for line in f:
-            inpt = line.replace('\n', '')
+            line = line.replace('\n', '')
+            inpt.append(line)
 
     return inpt
             
@@ -20,17 +21,65 @@ def getInput():
 def solution1():
     inpt = getInput()
     
+    ans = 0
+    for line in inpt:
+        i = 0
+        while i < len(line):
+            if i+4 < len(line) and line[i:i+4] == 'mul(':
+                endInd = i+4
+                while endInd < len(line) and line[endInd] in "01234567890,)":
+                    if line[endInd] == ')':
+                        endInd += 1
+                        break
+                    endInd += 1
+                testing and print("======", line[i:endInd])
+                if line[endInd-1] == ')':
+                    mulStr = line[i+4:endInd-1]
+                    mulStr = mulStr.replace(')','').split(',')
+                    if len(mulStr) == 2:
+                        testing and print("\t====", mulStr, '    =', int(mulStr[0]) * int(mulStr[1]))
+                        ans += int(mulStr[0]) * int(mulStr[1])
+            i += 1
 
-
-    return
+    return ans
 
 
 def solution2():
     inpt = getInput()
 
+    mulEnabled = True
+    
+    ans = 0
+    for line in inpt:
+        i = 0
+        while i < len(line):
+            if i+4 < len(line) and line[i:i+4] == 'mul(':
+                endInd = i+4
+                while endInd < len(line) and line[endInd] in "01234567890,)":
+                    if line[endInd] == ')':
+                        endInd += 1
+                        break
+                    endInd += 1
+                testing and print("======", line[i:endInd])
+                if line[endInd-1] == ')':
+                    mulStr = line[i+4:endInd-1]
+                    mulStr = mulStr.replace(')','').split(',')
+                    if len(mulStr) == 2:
+                        testing and print("\t====", mulStr, '    =', int(mulStr[0]) * int(mulStr[1]))
+                        if mulEnabled:
+                            ans += int(mulStr[0]) * int(mulStr[1])
+                        else:
+                            testing and print("Can't add. Mul is disabled")
+            elif line[i] == 'd':
+                if i+6 < len(line) and line[i:i+7] == 'don\'t()':
+                    testing and print("Mul disabled")
+                    mulEnabled = False
+                elif i+3 < len(line) and line[i:i+4] == 'do()':
+                    testing and print("Mul enabled")
+                    mulEnabled = True
+            i += 1
 
-
-    return
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
