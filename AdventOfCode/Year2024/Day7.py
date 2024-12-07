57976ee7203c889992799c314c7487ea9cf8dfe6
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-06 23:38:26 -0600

    Completed year 2024 day 7 both parts, but again I think I want to optimize my solutions. Part 2 in particular takes way too long to output the correct answer

--- AdventOfCode/Year2024/Day7.py
+++ AdventOfCode/Year2024/Day7.py
@@ -8,11 +8,15 @@ else:
 
 
 def getInput():
-    inpt = ""
+    inpt = []
 
     with open(inFile, 'r') as f:
         for line in f:
-            inpt = line.replace('\n', '')
+            line = line.replace('\n', '')
+            end, nums = line.split(': ')
+            end = int(end)
+            nums = [int(x) for x in nums.split(' ')]
+            inpt.append([end, nums])
 
     return inpt
             
@@ -20,17 +24,69 @@ def getInput():
 def solution1():
     inpt = getInput()
     
+    def recursiveEquation(testVal_:int, nums_:list, ops_:list=[])->bool:
+        '''Performs a recursive equation where each permutation of operators (addition, multiplication)
+        are used between each of the given list of numbers to see if it equates to the given test value.
+        '''
+        if len(ops_) == len(nums_)-1:
+            testing and print('\t', nums_, ops_, "  ==  ", testVal_)
+            result = nums_[0]
+            for i in range(len(ops_)):
+                if ops_[i] == '+':
+                    result += nums_[i+1]
+                elif ops_[i] == '*':
+                    result *= nums_[i+1]
+            return result == testVal_
 
+        opsPlus = [x for x in ops_]
+        opsPlus.append('+')
+        opsMult = [x for x in ops_]
+        opsMult.append('*')
+        return (recursiveEquation(testVal_, nums_, opsPlus) or recursiveEquation(testVal_, nums_, opsMult))
 
-    return
+    #Performing the recursive evaluation on each line of the input
+    ans = 0
+    for line in inpt:
+        if recursiveEquation(line[0], line[1], []):
+            ans += line[0]
+
+    return ans
 
 
 def solution2():
     inpt = getInput()
+    
+    def recursiveEquation(testVal_:int, nums_:list, ops_:list=[])->bool:
+        '''Performs a recursive equation where each permutation of operators (addition, multiplication, concatination)
+        are used between each of the given list of numbers to see if it equates to the given test value.
+        '''
+        if len(ops_) == len(nums_)-1:
+            result = nums_[0]
+            for i in range(len(ops_)):
+                if ops_[i] == '+':
+                    result += nums_[i+1]
+                elif ops_[i] == '*':
+                    result *= nums_[i+1]
+                elif ops_[i] == '|':
+                    result = int(str(result) + str(nums_[i+1]))
+            testing and print('\t', nums_, ops_, "=", result, "    ==", testVal_, "?")
+            return result == testVal_
 
+        opsPlus = [x for x in ops_]
+        opsPlus.append('+')
+        opsMult = [x for x in ops_]
+        opsMult.append('*')
+        opsCnct = [x for x in ops_]
+        opsCnct.append('|')
+        return (recursiveEquation(testVal_, nums_, opsPlus) or recursiveEquation(testVal_, nums_, opsMult) or recursiveEquation(testVal_, nums_, opsCnct))
 
+    #Performing the recursive evaluation on each line of the input
+    ans = 0
+    for line in inpt:
+        if recursiveEquation(line[0], line[1], []):
+            ans += line[0]
 
-    return
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
