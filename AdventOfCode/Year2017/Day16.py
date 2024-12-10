1631f014ebb21fd8c079e6161f8bd5f2a2766b61
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-10 12:47:58 -0600

    Went back and completed year 2017 day 16 part 2 that I was putting off. Trying to finish up the last 3 problems remaining for year 2017

--- AdventOfCode/Year2017/Day16.py
+++ AdventOfCode/Year2017/Day16.py
@@ -56,8 +56,76 @@ def solution1():
 
 def solution2():
     inpt = getInput()
-    
-    return
+    dancers = [x for x in "abcdefghijklmnop"]
+    if testing:
+        dancers = dancers[:5]
+        print("Dancers:", dancers)
+
+    #Looping until we find a pattern that has been seen before
+    danceOrders = {''.join(dancers):[0]} #Key = string for the order of the dancers, Value = dance iteration when seen
+    fullCycle = -1 #number of loops at which the dance completes a full cycle and returns to the starting configuration
+    for i in range(10000):
+        for move in inpt:
+            if move[0] == 's': #spin
+                spinNum = int(move[1:])
+                frontDancers = dancers[:len(dancers)-spinNum]
+                dancers = dancers[len(dancers)-spinNum:]
+                dancers.extend(frontDancers)
+            
+            elif move[0] == 'x': #exchange
+                move = move.replace('x','').split('/')
+                index1 = int(move[0])
+                index2 = int(move[1])
+                placeholder = dancers[index1]
+                dancers[index1] = dancers[index2]
+                dancers[index2] = placeholder
+            
+            else: #partner
+                d1 = move[1]
+                d1ind = dancers.index(d1)
+                d2 = move[3]
+                d2ind = dancers.index(d2)
+                dancers[d1ind] = d2
+                dancers[d2ind] = d1
+
+        curOrder = ''.join(dancers)
+        if curOrder in danceOrders.keys():
+            danceOrders[curOrder].append(i+1)
+            testing and print("\tOrder", curOrder, "seen again in i =", i+1)
+            if len(danceOrders[curOrder]) == 5:
+                testing and print("\t\tOrder", curOrder, "was seen in i =", danceOrders[curOrder])
+                fullCycle = danceOrders[curOrder][1] - danceOrders[curOrder][0]
+                testing and print("\t\tDance repeats every", fullCycle, "cycles. 1000000000 %", fullCycle, "=", 1000000000 % fullCycle)
+                break
+        else:
+            danceOrders[curOrder] = [i+1]
+
+    #Performing the dance for only the last loop
+    for i in range(1000000000 % fullCycle):
+        for move in inpt:
+            if move[0] == 's': #spin
+                spinNum = int(move[1:])
+                frontDancers = dancers[:len(dancers)-spinNum]
+                dancers = dancers[len(dancers)-spinNum:]
+                dancers.extend(frontDancers)
+            
+            elif move[0] == 'x': #exchange
+                move = move.replace('x','').split('/')
+                index1 = int(move[0])
+                index2 = int(move[1])
+                placeholder = dancers[index1]
+                dancers[index1] = dancers[index2]
+                dancers[index2] = placeholder
+            
+            else: #partner
+                d1 = move[1]
+                d1ind = dancers.index(d1)
+                d2 = move[3]
+                d2ind = dancers.index(d2)
+                dancers[d1ind] = d2
+                dancers[d2ind] = d1
+
+    return ''.join(dancers)
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
