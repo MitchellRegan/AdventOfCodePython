46c4f5522cfd8079b35c468cbc82642239d330d0
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-09 23:20:54 -0600

    Completed year 2024 day 10 both parts

--- AdventOfCode/Year2024/Day10.py
+++ AdventOfCode/Year2024/Day10.py
@@ -8,66 +8,92 @@ else:
 
 
 def getInput():
-    inpt = ""
+    inpt = []
 
     with open(inFile, 'r') as f:
         for line in f:
-            inpt = line.replace('\n', '')
+            line = line.replace('\n', '')
+            inpt.append([int(x) for x in line])
 
     return inpt
             
 
 def solution1():
     inpt = getInput()
-    testing and print("Input string:", inpt)
     
-    for x in range(40):
-        nextInpt = ""
-        i = 0
-        while i < len(inpt):
-            sameDigit = 1
-            j = i+1
-            while j < len(inpt):
-                if inpt[j] == inpt[i]:
-                    sameDigit += 1
-                    j += 1
-                else:
-                    break
-            testing and print("\t", inpt[i:j], "becomes", j-i, inpt[i])
-            nextInpt = nextInpt + str(j-i) + inpt[i]
-            i = j
-        inpt = nextInpt
+    startPoints = []
+    for r in range(len(inpt)):
+        for c in range(len(inpt[r])):
+            if inpt[r][c] == 0:
+                startPoints.append((r,c))
+    testing and print("Start Points:", startPoints)
 
-    return len(inpt)
+    ans = 0
+    for sp in startPoints:
+        q = [sp] #Each element is (row, col)
+        score = 0
+        reachedNines = []
 
-
-from collections import deque
-def nextLookAndSaySeq(lass_:str):
-    '''Takes a Look-And-Say-Sequence (LASS) string and returns the next sequence string.'''
-    nextS = deque()
-    i = 0
-    while i < len(lass_):
-        sameDigit = 1
-        j = i+1
-        while j < len(lass_):
-            if lass_[j] == lass_[i]:
-                sameDigit += 1
-                j += 1
+        while len(q) > 0:
+            r,c = q.pop(0)
+            val = inpt[r][c]
+            if val == 9 and (r,c) not in reachedNines:
+                score += 1
+                reachedNines.append((r,c))
             else:
-                break
-        nextS.append(str(j-i))
-        nextS.append(lass_[i])
-        i = j
-    return ''.join(nextS)
+                #Checking up
+                if r > 0 and inpt[r-1][c] - val == 1:
+                    q.append((r-1,c))
+                #Checking down
+                if r < len(inpt)-1 and inpt[r+1][c] - val == 1:
+                    q.append((r+1,c))
+                #Checking left
+                if c > 0 and inpt[r][c-1] - val == 1:
+                    q.append((r,c-1))
+                #Checking right
+                if c < len(inpt[r])-1 and inpt[r][c+1] - val == 1:
+                    q.append((r,c+1))
+        testing and print("Starting Point", sp, "score =", score)
+        ans += score
+    return ans
 
 
 def solution2():
     inpt = getInput()
     
-    for x in range(50):
-        inpt = nextLookAndSaySeq(inpt)
+    startPoints = []
+    for r in range(len(inpt)):
+        for c in range(len(inpt[r])):
+            if inpt[r][c] == 0:
+                startPoints.append((r,c))
+    testing and print("Start Points:", startPoints)
 
-    return len(inpt)
+    ans = 0
+    for sp in startPoints:
+        q = [sp] #Each element is (row, col)
+        score = 0
+
+        while len(q) > 0:
+            r,c = q.pop(0)
+            val = inpt[r][c]
+            if val == 9:
+                score += 1
+            else:
+                #Checking up
+                if r > 0 and inpt[r-1][c] - val == 1:
+                    q.append((r-1,c))
+                #Checking down
+                if r < len(inpt)-1 and inpt[r+1][c] - val == 1:
+                    q.append((r+1,c))
+                #Checking left
+                if c > 0 and inpt[r][c-1] - val == 1:
+                    q.append((r,c-1))
+                #Checking right
+                if c < len(inpt[r])-1 and inpt[r][c+1] - val == 1:
+                    q.append((r,c+1))
+        testing and print("Starting Point", sp, "score =", score)
+        ans += score
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
