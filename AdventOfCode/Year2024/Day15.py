8ad8b919a6923c4c1cffcee5b7db7681052c0810
MitchRegan <mitchell.p.regan@gmail.com>
2024-12-15 17:56:59 -0600

    Completed year 2024 day 15 part 2

--- AdventOfCode/Year2024/Day15.py
+++ AdventOfCode/Year2024/Day15.py
@@ -1,6 +1,6 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
@@ -87,16 +87,85 @@ def solution2():
 
     r,c = startPos
     c *= 2
-    
-    testing and print("Grid:")
-    for row in grid:
-        print(''.join(row))
-    testing and print("\nStart Pos:", (r,c), "\n\nMoves:")
-    print(moves)
-    return
+
+    def moveBoxHorizontal(boxRow_:int, boxLeftCol_:int, colOffset_:int)->bool:
+        '''Recursive function to try moving a box in the offset direction. Goes off of the LEFT '[' square bracket
+        (row,col) position of a box. Returns True if it was moved.'''
+
+        if colOffset_ == -1: #move left
+            if grid[boxRow_][boxLeftCol_-1] == '#':
+                return False
+            if grid[boxRow_][boxLeftCol_-1] == '.' or moveBoxHorizontal(boxRow_, boxLeftCol_-2, colOffset_):
+                grid[boxRow_][boxLeftCol_-1] = '['
+                grid[boxRow_][boxLeftCol_] = ']'
+                grid[boxRow_][boxLeftCol_+1] = '.'
+                return True
+            return False
+        else: #move right
+            if grid[boxRow_][boxLeftCol_+2] == '#':
+                return False
+            if grid[boxRow_][boxLeftCol_+2] == '.' or moveBoxHorizontal(boxRow_, boxLeftCol_+2, colOffset_):
+                grid[boxRow_][boxLeftCol_+1] = '['
+                grid[boxRow_][boxLeftCol_+2] = ']'
+                grid[boxRow_][boxLeftCol_] = '.'
+                return True
+            return False
+
+
+    def canMoveBoxVertical(r_:int, c_:int, rowOffset_:int)->bool:
+        '''Recursive call to check if a box can be moved vertically (DOES NOT ACTUALLY MOVE THE BOX)'''
+
+        #Defining the (row,col) position for the left '[' and right ']' sides of the box for readability
+        leftCol = c_
+        rightCol = c_+1
+        if grid[r_][c_] == ']':
+            leftCol = c_-1
+            rightCol = c_
+
+        if grid[r_+rowOffset_][leftCol] == '.' and grid[r_+rowOffset_][rightCol] == '.':
+            return True
+        
+        if grid[r_+rowOffset_][leftCol] == '#' or grid[r_+rowOffset_][rightCol] == '#':
+            return False
+
+        if grid[r_+rowOffset_][leftCol] == '[':
+            return canMoveBoxVertical(r_+rowOffset_, leftCol, rowOffset_)
+
+        otherBox1 = True 
+        if grid[r_+rowOffset_][leftCol] == ']':
+            otherBox1 = canMoveBoxVertical(r_+rowOffset_, leftCol, rowOffset_)
+        otherBox2 = True
+        if grid[r_+rowOffset_][rightCol] == '[':
+            otherBox2 = canMoveBoxVertical(r_+rowOffset_, rightCol, rowOffset_)
+        return otherBox1 and otherBox2
+
+
+    def moveBoxVertical(r_:int, c_:int, rowOffset_:int):
+        '''Recursive call to move this box and all boxes it runs into.'''
+
+        #Defining the (row,col) position for the left '[' and right ']' sides of the box for readability
+        leftCol = c_
+        rightCol = c_+1
+        if grid[r_][c_] == ']':
+            leftCol = c_-1
+            rightCol = c_
+
+        if grid[r_+rowOffset_][leftCol] == '[':
+            moveBoxVertical(r_+rowOffset_, leftCol, rowOffset_)
+        elif grid[r_+rowOffset_][leftCol] == ']':
+            moveBoxVertical(r_+rowOffset_, leftCol, rowOffset_)
+            if grid[r_+rowOffset_][rightCol] == '[':
+                moveBoxVertical(r_+rowOffset_, rightCol, rowOffset_)
+        elif grid[r_+rowOffset_][rightCol] == '[':
+            moveBoxVertical(r_+rowOffset_, rightCol, rowOffset_)
+
+        grid[r_+rowOffset_][leftCol] = '['
+        grid[r_+rowOffset_][rightCol] = ']'
+        grid[r_][leftCol] = '.'
+        grid[r_][rightCol] = '.'
+        return
 
     for dir in moves:
-        testing and print("\nDirection:", dir)
         offset = (0,0)
         if dir == '^':
             offset = (-1,0)
@@ -116,18 +185,17 @@ def solution2():
             c += offset[1]
         #Hitting a box attempts to move all boxes in a line
         elif grid[r+offset[0]][c+offset[1]] == '[' or grid[r+offset[0]][c+offset[1]] == ']':
-            targetRow = r + offset[0]
-            targetCol = c + offset[1]
-            while grid[targetRow][targetCol] == '[' or grid[targetRow][targetCol] == ']':
-                targetRow += offset[0]
-                targetCol += offset[1]
-
-            #If the row of boxes can be moved to an empty space, we move them all
-            if grid[targetRow][targetCol] == '.':
-                grid[r+offset[0]][c+offset[1]] = '.'
-                grid[targetRow][targetCol] = 'O'
+            if dir == '>' and moveBoxHorizontal(r, c+1, 1):
                 r += offset[0]
                 c += offset[1]
+            elif dir == '<' and moveBoxHorizontal(r, c-2, -1):
+                r += offset[0]
+                c += offset[1]
+            elif dir == 'v' or dir == '^':
+                if canMoveBoxVertical(r+offset[0], c, offset[0]):
+                    moveBoxVertical(r+offset[0], c, offset[0])
+                    r += offset[0]
+                    c += offset[1]
 
         if testing:
             displayGrid = []
