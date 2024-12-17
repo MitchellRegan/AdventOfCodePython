05a1eb4bff6bc0331e6d493e5873a2da982ed71f
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-17 02:48:09 -0600

    Completed year 2024 day 17 part 1 but not part 2. Dabbled a bit more with year 2024 day 16 part 2 but not completed

--- AdventOfCode/Year2024/Day16.py
+++ AdventOfCode/Year2024/Day16.py
@@ -80,87 +80,86 @@ def solution1():
     return
 
 
+from collections import deque
 def solution2():
     grid, startPos, endPos = getInput()
     
-    q = [(startPos, '>', 0)] #Each element is ((row,col) position, direction facing, current score)
-    seen = {} #Key = (row,col) position, value = score
-    prev = {startPos:None} #Key = (row,col) position, value = (row,col) of the previous tile in the path
-    multiHit = {} #Key = (row,col) position, value = ((row,col) of an intersection where scores were tied, score when hit)
+    #Finding every tile that's an intersection, corner, or dead-end
+    intersections = {startPos:[], endPos:[]} #Key = (row,col) position, Value = [list of connected intersections and int for distance between]
+    for row in range(len(grid)):
+        grid[row] = [x for x in grid[row]]
+        for col in range(len(grid[row])):
+            if grid[row][col] == '.':
+                adj = [grid[r][c] for (r,c) in [(row+1,col), (row-1,col), (row,col+1), (row,col-1)]].count('#')
+                if adj != 2 or (adj == 2 and grid[row+1][col] != grid[row-1][col] and grid[row][col+1] != grid[row][col-1]):
+                    grid[row][col] = 'I'
+                    intersections[(row,col)] = []
+        testing and print(''.join(grid[row]).replace('#', ' '))
 
-    bestScore = None
+    #Performing a bfs from each intersection point to get the distances between connected intersections
+    dists = {}#Key = (pair of (r,c) pos for connected intersections), Value = int for distance between them
+    for i in intersections.keys():
+        q = deque()
+        q.append(i)
+        seen = {i:0}#Key = (r,c) pos of tiles found, Value = distance from the starting intersection tile
+
+        while len(q) > 0:
+            r,c = q.popleft()
+
+            #If another intersection point is found, we track the edge connection as well as the distance
+            if grid[r][c] == 'I' and (r,c) != i:
+                if (r,c) not in intersections[i]:
+                    dists[(min(i, (r,c)), max(i, (r,c)))] = seen[(r,c)]
+                    intersections[i].append((r,c))
+
+            #Otherwise we keep spreading the bfs
+            else:
+                for adj in [(r+1,c), (r-1,c), (r,c+1), (r,c-1)]:
+                    if grid[adj[0]][adj[1]] != '#' and adj not in seen.keys():
+                        seen[adj] = seen[i]+1
+                        q.append(adj)
+
+    for k in intersections.keys():
+        print(k, ":", intersections[k])
+
+    return
+    
+    q = deque()
+    q.append((startPos, startPos)) #Each element is ((row,col) position, and (row,col) position of previous intersection visited)
+    seen = {} #Key = (row,col) pos, Value = ((row,col) for previous intersection seen, int for distance from intersection)
+
     while len(q) > 0:
-        pos, dir, curScore = q.pop(0)
+        pos, prevInt = q.popleft()
+        r,c = pos
+        testing and print(r, c)
 
-        #If we found the end position, this is definitionally the lowest score since we sort the que by lowest score
-        if pos == endPos and (bestScore is None or bestScore > curScore):
-            bestScore = curScore
-            #break
+        #If this tile is at an intersection, corner, or dead-end, we track it
+        isIntersection = False
+        if grid[r-1][c] != grid[r+1][c] and grid[r][c-1] != grid[r][c+1]:
+            isIntersection = True
+            testing and print("\tIs intersection")
+            if pos not in intersections.keys():
+                intersections[pos] = [(prevInt, seen[pos][1])]
+            else:
+                intersections[pos].append((prevInt, seen[pos][1]))
 
-        validDirs = []
+        #Checking adjacent tiles for BFS
+        for a in [(r-1,c), (r+1,c), (r,c-1), (r,c+1)]:
+            if grid[a[0]][a[1]] != '#':
+                openSpaces += 1
+                if a not in seen.keys():
+                    if isIntersection:
+                        q.append((a, pos))
+                    else:
+                        q.append((a, prevInt))
 
-        if dir == '>':
-            validDirs = [('>', 0, 1), ('v', 1, 0), ('^', -1, 0)]
-        elif dir == '<':
-            validDirs = [('<', 0, -1), ('v', 1, 0), ('^', -1, 0)]
-        elif dir == 'v':
-            validDirs = [('>', 0, 1), ('<', 0, -1), ('v', 1, 0)]
-        elif dir == '^':
-            validDirs = [('>', 0, 1), ('<', 0, -1), ('^', -1, 0)]
+        if isIntersection:
+            seen[pos] = (pos, 0)
+        else:
+            seen[pos] = (prevInt, seen[prevInt][2])
 
-        for nextDir in validDirs:
-            #testing and print("\tTrying dir", nextDir)
-            nextScore = curScore
-            nextRow = pos[0] + nextDir[1]
-            nextCol = pos[1] + nextDir[2]
 
-            #Moving in this direction is only valid if it's not blocked by a wall
-            if grid[nextRow][nextCol] == '.':
-                #Travelling in the same direction only adds 1 to the score
-                if nextDir[0] == dir:
-                    nextScore += 1
-                #Making a turn 90 degrees adds 1000 to the score
-                else:
-                    nextScore += 1001
-
-                if (nextRow, nextCol) not in seen.keys() or seen[(nextRow, nextCol)] > nextScore:
-                    #if (nextRow, nextCol) in seen.keys() and seen[(nextRow, nextCol)] == nextScore:
-                    #    multiTile = [x for x in prev[(nextRow, nextCol)]].append(pos)
-                    #    prev[(nextRow, nextCol)]
-                    #else:
-                    prev[(nextRow, nextCol)] = pos
-                    seen[(nextRow, nextCol)] = nextScore
-                    #testing and print("\t\tadding element:", ((nextRow, nextCol), nextDir[0], nextScore))
-                    q.append(((nextRow, nextCol), nextDir[0], nextScore))
-                    #Sorting the que so that the lowest score always goes first (Dijkstra's algorithm)
-                    q.sort(key = lambda x: x[2])
-                elif (nextRow, nextCol) in seen.keys() and seen[(nextRow, nextCol)] == nextScore:
-                    multiHit[pos] = ((nextRow, nextCol), nextScore)
-
-    if testing:
-        print()
-        for r in range(len(grid)):
-            grid[r] = [x for x in grid[r]]
-
-    ans = 1
-    tile = endPos
-    while tile is not None:
-        if testing:
-            grid[tile[0]][tile[1]] = 'O'
-        tile = prev[tile]
-        ans += 1
-
-    for mh in multiHit.keys():
-        grid[mh[0]][mh[1]] = '?'
-        print("==== Multi-hit at", mh)
-
-    if testing:
-        for row in grid:
-            prow = ''.join(row)
-            prow = prow.replace('.', ' ').replace('O', '.')
-            print(prow)
-        print()
-    return ans 
+    return
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
--- AdventOfCode/Year2024/Day17.py
+++ AdventOfCode/Year2024/Day17.py
@@ -1,36 +1,165 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 0
+testing = 1
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
-
+import math
+from collections import deque
 
 def getInput():
-    inpt = ""
+    regs = {}
+    prog = []
 
     with open(inFile, 'r') as f:
+        onRegs = True
         for line in f:
-            inpt = line.replace('\n', '')
+            line = line.replace('\n', '').split(' ')
 
-    return inpt
+            if line[0] == '':
+                onRegs = False
+            elif onRegs:
+                regs[line[1][0]] = int(line[2])
+            else:
+                prog = line[1].split(',')
+                prog = [int(x) for x in prog]
+
+    return regs, prog
             
 
 def solution1():
-    inpt = getInput()
-    
+    regs, prog = getInput()
+    output = ""
 
+    iptr = 0
+    while iptr < len(prog)-1:
+        #Literal operand is the value in the program just after the instruction pointer
+        literalOp = prog[iptr+1]
+        #Combo operand has weird instructions based on what the literalOp is
+        comboOp = literalOp
+        if literalOp == 4:
+            comboOp = regs['A']
+        elif literalOp == 5:
+            comboOp = regs['B']
+        elif literalOp == 6:
+            comboOp = regs['C']
 
-    return
+        if prog[iptr] == 0: #adv performs division of reg A and combo
+            regs['A'] = math.trunc(regs['A'] / (2**comboOp))
+
+        elif prog[iptr] == 1: #bxl performs bitwise XOR of reg B and literal
+            regs['B'] = regs['B'] ^ literalOp
+
+        elif prog[iptr] == 2: #bst performs input val mod 8
+            regs['B'] = comboOp % 8
+
+        elif prog[iptr] == 3: #jnz jumps iptr if reg A != 0, otherwise does nothing
+            if regs['A'] != 0:
+                iptr = literalOp - 2 #The -2 offsets the default +2 after each loop
+
+        elif prog[iptr] == 4: #bxc calculates bitwise XOR of reg B and reg C
+            regs['B'] = regs['B'] ^ regs['C']
+
+        elif prog[iptr] == 5: #out calculates the value of combo mod 8 and adds it to the output list
+            if output == "":
+                output = str(comboOp % 8)
+            else:
+                output = output + ',' + str(comboOp % 8)
+
+        elif prog[iptr] == 6: #bdv performs division of reg B and combo
+            regs['B'] = math.trunc(regs['A'] / (2**comboOp))
+
+        elif prog[iptr] == 7: #cdv
+            regs['C'] = math.trunc(regs['A'] / (2**comboOp))
+
+        iptr += 2
+
+    return output
 
 
 def solution2():
-    inpt = getInput()
+    startingRegs, startingProg = getInput()
+    print("List prog: ", startingProg)
+    prog = deque()
+    for x in startingProg:
+        prog.append(x)
+    print("Deque prog:", prog)
 
+    newA = 0 #500170000 #Tried up until 100,000,000 no luck
+    jumpSize = 7
+    if testing:
+        newA = 0
+    longestOutput = 0
+    while True:
+        if newA % 500000 == 0:
+            print("Testing A =", newA)
 
+        regs = {}
+        for x in startingRegs.keys():
+            regs[x] = startingRegs[x]
+        regs['A'] = newA
+        output = deque()
+        isValid = True
 
-    return
+        iptr = 0
+        while iptr < len(prog)-1:
+            #Literal operand is the value in the program just after the instruction pointer
+            literalOp = prog[iptr+1]
+            #Combo operand has weird instructions based on what the literalOp is
+            comboOp = literalOp
+            if literalOp == 4:
+                comboOp = regs['A']
+            elif literalOp == 5:
+                comboOp = regs['B']
+            elif literalOp == 6:
+                comboOp = regs['C']
+
+            if prog[iptr] == 0: #adv performs division of reg A and combo
+                regs['A'] = math.trunc(regs['A'] / (2**comboOp))
+
+            elif prog[iptr] == 1: #bxl performs bitwise XOR of reg B and literal
+                regs['B'] = regs['B'] ^ literalOp
+
+            elif prog[iptr] == 2: #bst performs input val mod 8
+                regs['B'] = comboOp % 8
+
+            elif prog[iptr] == 3: #jnz jumps iptr if reg A != 0, otherwise does nothing
+                if regs['A'] != 0:
+                    iptr = literalOp - 2 #The -2 offsets the default +2 after each loop
+
+            elif prog[iptr] == 4: #bxc calculates bitwise XOR of reg B and reg C
+                regs['B'] = regs['B'] ^ regs['C']
+
+            elif prog[iptr] == 5: #out calculates the value of combo mod 8 and adds it to the output list
+                output.append(comboOp % 8)
+                if prog[len(output)-1] != output[-1]:
+                    isValid = False
+                    break
+                elif len(output) > longestOutput:
+                    longestOutput = len(output)
+                    print("========== A =", newA, "longest output so far:", longestOutput)
+
+            elif prog[iptr] == 6: #bdv performs division of reg B and combo
+                regs['B'] = math.trunc(regs['A'] / (2**comboOp))
+
+            elif prog[iptr] == 7: #cdv
+                regs['C'] = math.trunc(regs['A'] / (2**comboOp))
+
+            iptr += 2
+
+        #If the program successfully completes, we have to double-check the output against the program input
+        if isValid and output == prog:
+            testing and print("\tA =", newA, "is VALID:", output)
+            if jumpSize > 1:
+                newA -= (jumpSize - 1)
+                jumpSize = 1
+            else:
+                newA += jumpSize
+        else:
+            newA += jumpSize
+
+    return newA
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
