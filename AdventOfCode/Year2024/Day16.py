48f3e2d287bebdc7457f986d689999f5081d2515
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-17 23:25:09 -0600

    Completed year 2024 day 18 both parts. Did a little bit of tweaking with day 16 part 2 but still not complete

--- AdventOfCode/Year2024/Day16.py
+++ AdventOfCode/Year2024/Day16.py
@@ -1,6 +1,6 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 1
+testing = 0
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
@@ -82,6 +82,13 @@ def solution1():
 
 def bfsIntersectionMap(startPos:tuple, endPos:tuple, intersections:dict, removed:list=[]):
     '''Performs a BFS through our map of connected intersection nodes.
+
+    Parameters
+    ----------
+        startPos: (row, col) position of the starting location.
+        endPos: (row, col) position of the destination point to reach.
+        intersections: Dictionary where Key = (row, col) for every intersection in the map, and Value = [list of connected intersections]
+        removed: List of (row, col) positions to ignore.
     
     Returns
     ----------
@@ -91,34 +98,39 @@ def bfsIntersectionMap(startPos:tuple, endPos:tuple, intersections:dict, removed
     shortestPath = deque()
     minimalScore = None
     q = deque()
-    q.append(startPos)
-    seen = {startPos:(0, None)} #Key = (r,c) pos of intersections found, Value = (score from start, prev intersection)
+    q.append((startPos, 0)) #Each element is ((row,col) pos, score from start)
+    seen = {startPos:None} #Key = (r,c) pos of intersections found, Value = prev intersection
     while len(q) > 0:
-        head = q.popleft()
+        head, curScore = q.popleft()
         #testing and print("Checking", head)
 
         if head == endPos:
-            minimalScore = seen[head][0]
+            minimalScore = curScore
             while head is not None:
                 shortestPath.appendleft(head)
-                head = seen[head][1]
+                head = seen[head]
             break
 
         for adj in intersections[head]:
             #testing and print("\tAdj:", adj)
-            if adj not in seen.keys():
+            if adj in removed:
+                continue
+            elif adj not in seen.keys():
                 d = abs(adj[0] - head[0]) + abs(adj[1] - head[1])
                 #Checking to see if any 90 degree turns would be made between the adjacent pos, the head pos, and the head's previous pos
-                if seen[head][1] is not None:
-                    rdiff = seen[head][1][0] - adj[0]
-                    cdiff = seen[head][1][1] - adj[1]
+                if seen[head] is not None:
+                    rdiff = seen[head][0] - adj[0]
+                    cdiff = seen[head][1] - adj[1]
                     if rdiff != 0 and cdiff != 0:
                         d += 1000
                 #Since the starting direction is '>' if the tile is adjacent to the start and above it, it counts as a turn
                 elif head == startPos and head[0] > adj[0]:
                     d += 1000
-                seen[adj] = (d+seen[head][0], head)
-                q.append(adj)
+                seen[adj] = head
+                q.append((adj, d+curScore))
+                q = deque(sorted(q, key=lambda x: x[1]))
+
+    return shortestPath, minimalScore
 
 
 def solution2():
@@ -161,49 +173,28 @@ def solution2():
                         q.append(adj)
 
     #Finding the most optimal path to the end point using dijkstra's algorithm
-    shortestPath = deque()
-    minimalScore = None
-    q = deque()
-    q.append(startPos)
-    seen = {startPos:(0, None)} #Key = (r,c) pos of intersections found, Value = (score from start, prev intersection)
-    while len(q) > 0:
-        head = q.popleft()
-        #testing and print("Checking", head)
-
-        if head == endPos:
-            minimalScore = seen[head][0]
-            while head is not None:
-                shortestPath.appendleft(head)
-                head = seen[head][1]
-            break
-
-        for adj in intersections[head]:
-            #testing and print("\tAdj:", adj)
-            if adj not in seen.keys():
-                d = abs(adj[0] - head[0]) + abs(adj[1] - head[1])
-                #Checking to see if any 90 degree turns would be made between the adjacent pos, the head pos, and the head's previous pos
-                if seen[head][1] is not None:
-                    rdiff = seen[head][1][0] - adj[0]
-                    cdiff = seen[head][1][1] - adj[1]
-                    if rdiff != 0 and cdiff != 0:
-                        d += 1000
-                #Since the starting direction is '>' if the tile is adjacent to the start and above it, it counts as a turn
-                elif head == startPos and head[0] > adj[0]:
-                    d += 1000
-                seen[adj] = (d+seen[head][0], head)
-                q.append(adj)
-
-    print("Minimal Score:", minimalScore)
-    print("Shortest Path:", shortestPath)
+    shortestPath, minimalScore = bfsIntersectionMap(startPos, endPos, intersections)
+    allShortestPaths = [shortestPath]
+    bestEdges = []
     for x in range(len(shortestPath)-1):
-        d = abs(shortestPath[x][0] - shortestPath[x+1][0]) + abs(shortestPath[x][1] - shortestPath[x+1][1])
-        if x > 0:
-            rdiff = shortestPath[x-1][0] - shortestPath[x+1][0]
-            cdiff = shortestPath[x-1][1] - shortestPath[x+1][1]
-            if rdiff != 0 and cdiff != 0:
-                d += 1000
-        print("\t", shortestPath[x], "->", shortestPath[x+1], "   Distance:", d)
-    return
+        bestEdges.append((min(shortestPath[x], shortestPath[x+1]), max(shortestPath[x], shortestPath[x+1])))
+
+    #Performing multiple BFS on the grid while ignoring a different intersection each time in order to find all valid paths
+    for i in intersections.keys():
+        if i != startPos and i != endPos:
+            testPath, testScore = bfsIntersectionMap(startPos, endPos, intersections, [i])
+            if testScore is not None and testScore == minimalScore:
+                for x in range(len(testPath)-1):
+                    newEdge = (min(testPath[x], testPath[x+1]), max(testPath[x], testPath[x+1]))
+                    if newEdge not in bestEdges:
+                        bestEdges.append(newEdge)
+
+
+    ans = 0
+    for e in bestEdges:
+        print("\t", e, "   = ", abs(e[0][0] - e[1][0]) + abs(e[0][1] - e[1][1]))
+        ans += abs(e[0][0] - e[1][0]) + abs(e[0][1] - e[1][1])
+    return ans
 
 
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
--- AdventOfCode/Year2024/Day18.py
+++ AdventOfCode/Year2024/Day18.py
@@ -5,31 +5,88 @@ if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputRealFiles/d" + aocDate[1] + "_real.txt"
-
+from collections import deque
 
 def getInput():
-    inpt = ""
+    inpt = []
 
     with open(inFile, 'r') as f:
         for line in f:
-            inpt = line.replace('\n', '')
+            line = line.replace('\n', '').split(',')
+            inpt.append([int(x) for x in line])
 
     return inpt
             
 
 def solution1():
     inpt = getInput()
+    width = 70
+    height = 70
+    numBytes = 1024
+    if testing:
+        width = 6
+        height = 6
+        numBytes = 12
     
+    walls = {}
+    for w in range(numBytes):
+        x,y = inpt[w]
+        walls[(x,y)] = True
 
+    startPos = (0,0)
+    endPos = (width, height)
+    seen = {startPos:0} #Key = (x,y) position, Value = distance from start
+    q = deque()
+    q.append(startPos)
+    while len(q) > 0:
+        head = q.popleft()
+
+        if head == endPos:
+            return seen[head]
+
+        for adj in [(head[0]+1, head[1]), (head[0]-1, head[1]), (head[0], head[1]+1), (head[0], head[1]-1)]:
+            if adj[0] >= 0 and adj[0] <= width and adj[1] >=0 and adj[1] <= height:
+                if adj not in seen.keys() and adj not in walls.keys():
+                    q.append(adj)
+                    seen[adj] = seen[head]+1
 
     return
 
 
 def solution2():
     inpt = getInput()
+    width = 70
+    height = 70
+    if testing:
+        width = 6
+        height = 6
+    
+    walls = {} #Key = (x,y) position of blocked space, Value doesn't matter
 
+    for w in range(len(inpt)):
+        x,y = inpt[w]
+        walls[(x,y)] = True
+        isValid = False
 
+        startPos = (0,0)
+        endPos = (width, height)
+        seen = {startPos:0} #Key = (x,y) position, Value = distance from start
+        q = deque()
+        q.append(startPos)
+        while len(q) > 0:
+            head = q.popleft()
 
+            if head == endPos:
+                isValid = True
+                break
+
+            for adj in [(head[0]+1, head[1]), (head[0]-1, head[1]), (head[0], head[1]+1), (head[0], head[1]-1)]:
+                if adj[0] >= 0 and adj[0] <= width and adj[1] >=0 and adj[1] <= height:
+                    if adj not in seen.keys() and adj not in walls.keys():
+                        q.append(adj)
+                        seen[adj] = seen[head]+1
+        if not isValid:
+            return "" + str(x) + ',' + str(y)
     return
 
 
