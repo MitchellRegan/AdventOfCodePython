9dc51c4735630fc41fc63ea6ee08f1587c6d6190
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2024-12-23 20:40:12 -0600

    Completed year 2024 day 20 part 1 Completed year 2024 day 23 part 1

--- AdventOfCode/Year2024/Day20.py
+++ AdventOfCode/Year2024/Day20.py
@@ -66,20 +66,52 @@ def bfsCheat(grid_:list, start_:tuple, end_:tuple, numCheatSec_:int, cheatStartT
 
 
 def solution1():
-    inpt, startPos, endPos = getInput()
+    grid, startPos, endPos = getInput()
     
-    baselineTime = bfsCheat(inpt, startPos, endPos, 2, -2)
-    print("Baseline bfs time:", baselineTime)
+    #Perform BFS starting from the endPos and store the distance each tile is from the endPos using a dict of tile (r,c):dist
+    q = deque()
+    q.append(startPos)
+    tileDists = {startPos:0}
+    while len(q) > 0:
+        head = q.popleft()
+        r,c = head
 
+        for adj in [(r+1,c), (r-1,c), (r,c+1), (r,c-1)]:
+            if adj not in tileDists.keys() and grid[adj[0]][adj[1]] != '#':
+                q.append(adj)
+                tileDists[adj] = tileDists[head]+1
+
+    testing and print("End tile is at", endPos, "distance", tileDists[endPos], '\n')
+    #Iterate through all tiles in the "tileDists" dictionary
+    #   Check the tile's adjacent spaces for walls
+    #   If the opposite side of that wall is another tile with a smaller distance, we can find the cheat distance
     ans = 0
-    for t in range(0, baselineTime-100):
-        if t % 20 == 0:
-            print("Checking t =", t)
-        cheatTime = bfsCheat(inpt, startPos, endPos, 2, t)
-        savedTime = baselineTime - cheatTime
-        if savedTime >= 100:
-            ans += 1
-        testing and print("\tCheating at t =", t, "gives time of", cheatTime, "saving", savedTime, "sec")
+    for tile in tileDists.keys():
+        r,c = tile
+        dist = tileDists[tile]
+
+        for dir in [(-1,0), (1,0), (0,-1), (0,1)]:
+            cheatTile = None
+            #Adjacent tile in this direction is a wall
+            if grid[r+dir[0]][c+dir[1]] == '#':
+                #One space after that wall isn't a wall and isn't out of bounds
+                if r+(2*dir[0]) >= 0 and r+(2*dir[0]) <= len(grid)-1 \
+                and c+(2*dir[1]) >= 0 and c+(2*dir[1]) <= len(grid[0])-1 \
+                and grid[r+(2*dir[0])][c+(2*dir[1])] != '#':
+                    cheatTile = (r+(2*dir[0]), c+(2*dir[1]))
+                #Two spaces after that wall isn't a wall and isn't out of bounds
+                elif r+(3*dir[0]) >= 0 and r+(3*dir[0]) <= len(grid)-1 \
+                and c+(3*dir[1]) >= 0 and c+(3*dir[1]) <= len(grid[0])-1 \
+                and grid[r+(3*dir[0])][c+(3*dir[1])] != '#':
+                    cheatTile = (r+(3*dir[0]), c+(3*dir[1]))
+
+            #If a cheat tile was found and it's distance is shorter than the current tile's distance, CHEAT!
+            if cheatTile is not None and tileDists[cheatTile] > dist:
+                savedDist = tileDists[cheatTile] - dist
+                savedDist -= abs(r-cheatTile[0]) + abs(c-cheatTile[1])
+                testing and print("Cheating from", tile, "dist", tileDists[tile], "to", cheatTile, "dist", tileDists[cheatTile], "saves", savedDist)
+                if savedDist >= 100:
+                    ans += 1
 
     return ans
 
--- AdventOfCode/Year2024/Day23.py
+++ AdventOfCode/Year2024/Day23.py
@@ -1,6 +1,6 @@
 aocDate = [__file__.split('\\')[-2][4:], __file__.split('\\')[-1][3:-3]]
 inFile = ""
-testing = 0
+testing = 1
 if testing:
     inFile = '/'.join(__file__.split('\\')[:-1]) + "/InputTestFiles/d" + aocDate[1] + "_test.txt"
 else:
@@ -8,30 +8,69 @@ else:
 
 
 def getInput():
-    inpt = ""
+    inpt = {}
 
     with open(inFile, 'r') as f:
         for line in f:
-            inpt = line.replace('\n', '')
+            c1,c2 = line.replace('\n', '').split('-')
+
+            if c1 not in inpt.keys():
+                inpt[c1] = []
+            inpt[c1].append(c2)
+            if c2 not in inpt.keys():
+                inpt[c2] = []
+            inpt[c2].append(c1)
+
+    for k in inpt.keys():
+        inpt[k].sort()
 
     return inpt
             
 
 def solution1():
-    inpt = getInput()
+    cmap = getInput()
     
+    ans = 0
+    setsOfThree = []
+    for c in cmap.keys():
+        testing and print(c, ":", cmap[c])
+        for i in range(len(cmap[c])-1):
+            for j in range(i+1, len(cmap[c])):
+                if cmap[c][i] in cmap[cmap[c][j]]:
+                    compGroup = [c, cmap[c][i], cmap[c][j]]
+                    compGroup.sort()
+                    if compGroup not in setsOfThree:
+                        setsOfThree.append(compGroup)
+                        testing and print("\t", compGroup)
+                        #If any of the computers in this group have a name that begins with 't' it's added to our final answer
+                        if compGroup[0][0] == 't' or compGroup[1][0] == 't' or compGroup[2][0] == 't':
+                            ans += 1
 
-
-    return
+    return ans
 
 
 def solution2():
-    inpt = getInput()
-
-
+    cmap = getInput()
+    
+    ans = 0
+    setsOfThree = []
+    for c in cmap.keys():
+        print(c, ":", cmap[c])
+        #Checking every computer in this list against every other computer
+        mostConnections = 0
+        for i in range(len(cmap[c])):
+            numConnections = 1
+            for j in range(len(cmap[c])):
+                if i != j and cmap[c][j] in cmap[cmap[c][i]]:
+                    numConnections += 1
+                    print("\t", cmap[c][i], "connected to", cmap[c][j])
+            print("\t------------------")
+            if numConnections > mostConnections:
+                mostConnections = numConnections
+        print("\t\tNum connections:", mostConnections)
 
     return
 
 
-print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
+#print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 2:", solution2())
