0add56c5d38a3049988d4cae5dffa676e19579c1
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2025-04-16 17:53:46 -0500

    Finished year 2015 day 15 part 2

--- AdventOfCode/Year2015/Day15.py
+++ AdventOfCode/Year2015/Day15.py
@@ -57,7 +57,7 @@ def solution1():
         ans = best_score
         tsp[best_index] = tsp[best_index]+1
 
-    return ans
+    return ans, tsp
 
 
 def solution2():
@@ -65,42 +65,61 @@ def solution2():
     #List of each ingredient name, because if we keep using inpt.keys() we're not guaranteed the same order each time
     ing = [x for x in inpt.keys()]
 
-    start = tuple([0]*len(inpt))
-    seen = {start:True} #dictionary where key is a tuple of ingredient teaspoons to track which combinations we've already seen
-    q = [start]
-    ans = 0
+    def get_score_and_cal(tsp_:list)->list:
+        '''Temp function to find the total score and calorie count from a list of ingredient teaspoons.'''
+        cap = dur = flv = tex = cal = score = 0
+        for i in range(len(tsp_)):
+            cap += inpt[ing[i]][0] * tsp_[i]
+            dur += inpt[ing[i]][1] * tsp_[i]
+            flv += inpt[ing[i]][2] * tsp_[i]
+            tex += inpt[ing[i]][3] * tsp_[i]
+            cal += inpt[ing[i]][4] * tsp_[i]
 
-    #Performing what is essentially a BFS where we check all combinations that are 1 tsp removed from the current
-    #combination until we find ones that have exactly 100 tsp and 500 cal
+        #Totals for each value can't be negative, so they're calculated at 0 instead (per the instructions on y2015d15)
+        score = max(0,cap) * max(0,dur) * max(0,flv) * max(0,tex)
+        return score, cal
+    
+    #Getting the score and teaspoon configuration for part 1's answer to use as a starting point
+    p1_score, p1_tsp = solution1()
+    p1_score, p1_cal = get_score_and_cal(p1_tsp)
+
+    #Performing a BFS from our starting teaspoon configuration
+    seen = {tuple(p1_tsp):tuple(get_score_and_cal(p1_tsp))}
+    q = [tuple(p1_tsp)]
+    ans = -1
     while len(q) > 0:
         tsp = q.pop(0)
+        score, cal = seen[tsp]
 
-        cap = dur = flv = tex = cal = 0
-        #Finding the current values of each property
-        for i in range(len(ing)):
-            cap += (inpt[ing[i]][0] * tsp[i])
-            dur += (inpt[ing[i]][1] * tsp[i])
-            flv += (inpt[ing[i]][2] * tsp[i])
-            tex += (inpt[ing[i]][3] * tsp[i])
-            cal += (inpt[ing[i]][4] * tsp[i])
-
-        #Combinations with exactly 100 tsp of ingredients have their calorie count checked
-        if sum(tsp) == 100:
-            if cal == 500:
-                score = cap * dur * flv * tex
-                ans = max(ans, score)
-        #Combinations that have less than 100 tsp of ingredients are iterated on
+        #Saving the best score where the ingredients have exactly 500 calories
+        if cal == 500:
+            if score > ans:
+                ans = score
+            continue
         else:
-            for i in range(len(ing)):
-                new_combo = [x for x in tsp]
-                new_combo[i] += 1
-                new_combo = tuple(new_combo)
-                if new_combo not in seen.keys():
-                    seen[new_combo] = True
-                    q.append(new_combo)
+            #From the current configuration of teaspoons, we try shifting 1tsp from each group to every other group to see the result
+            for i in range(len(tsp)):
+                for j in range(len(tsp)):
+                    if i == j:
+                        continue
+                    #Shifting 1tsp from the ingredient at index i to the ingredient at index j
+                    new_tsp = [x for x in tsp]
+                    new_tsp[i] -= 1
+                    new_tsp[j] += 1
+                    new_tsp = tuple(new_tsp)
+
+                    if new_tsp not in seen.keys():
+                        new_score, new_cal = get_score_and_cal(new_tsp)
+                        #If the calorie count is higher than the answer from part 1, or the calorie is too far
+                        #below the goal of 500, or this teaspoon combo gives a score too far below our current
+                        #best answer, we don't add it to the que
+                        if new_cal < p1_cal and new_cal > 485 and (ans == -1 or new_score > (0.75*ans)):
+                            q.append(new_tsp)
+                            seen[new_tsp] = tuple(get_score_and_cal(new_tsp))
+
     return ans
 #not 17694720
 
 
-print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1())
+print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 1:", solution1()[0])
 print("Year " + aocDate[0] + " Day " + aocDate[1] + " solution part 2:", solution2())
